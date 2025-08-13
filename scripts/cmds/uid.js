c95a1044dac5820bc11463c1528fdde54de13130
kira <kirajanu76@gmail.com>
2025-08-13 19:46:55 +0600

    Update uid.js

--- scripts/cmds/uid.js
+++ scripts/cmds/uid.js
@@ -1,4 +1,5 @@
-module.exports.config = {
+module.exports = {
+  config: {
     name: "uid",
     version: "1.0.0",
     hasPermssion: 0,
@@ -6,18 +7,22 @@ module.exports.config = {
     description: "𝙐𝙨𝙚𝙧𝙚𝙧 𝙄𝘿 𝙟𝙖𝙣𝙩𝙚 𝙥𝙖𝙧𝙗𝙚𝙣",
     commandCategory: "𝙏𝙤𝙤𝙡𝙨",
     cooldowns: 5
-};
+  },
 
-module.exports.run = function({ api, event }) {
+  run: async function ({ api, event }) {
     if (Object.keys(event.mentions).length === 0) {
-        return api.sendMessage(`𝘼𝙥𝙣𝙖𝙧 𝙐𝙨𝙚𝙧 𝙄𝘿: ${event.senderID}`, event.threadID, event.messageID);
+      return api.sendMessage(
+        `𝘼𝙥𝙣𝙖𝙧 𝙐𝙨𝙚𝙧 𝙄𝘿: ${event.senderID}`,
+        event.threadID,
+        event.messageID
+      );
+    } else {
+      let msg = "";
+      for (const [id, name] of Object.entries(event.mentions)) {
+        const cleanName = name.replace('@', '');
+        msg += `${cleanName} - 𝙐𝙨𝙚𝙧 𝙄𝘿: ${id}\n`;
+      }
+      return api.sendMessage(msg, event.threadID, event.messageID);
     }
-    else {
-        let msg = "";
-        for (const [id, name] of Object.entries(event.mentions)) {
-            const cleanName = name.replace('@', '');
-            msg += `${cleanName} - 𝙐𝙨𝙚𝙧 𝙄𝘿: ${id}\n`;
-        }
-        return api.sendMessage(msg, event.threadID, event.messageID);
-    }
-}
+  }
+};
