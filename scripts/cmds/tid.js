7fd395955535518117f9ae225eea6643276bd2c1
kira <kirajanu76@gmail.com>
2025-08-13 21:23:32 +0600

    Update tid.js

--- scripts/cmds/tid.js
+++ scripts/cmds/tid.js
@@ -1,15 +1,27 @@
+"use strict";
+
+/**
+ * Command: tid
+ * Description: Sends the current group's/thread's ID.
+ * Credits: 𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅
+ */
+
 module.exports.config = {
-	name: "tid",	
-	version: "1.0.0", 
-	hasPermssion: 0,
-	credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
-	description: "𝙂𝙧𝙪𝙥𝙚𝙧 𝙞𝙙 𝙟𝙖𝙣𝙩𝙚 𝙘𝙝𝙖𝙞", 
-	commandCategory: "𝙂𝙧𝙪𝙥",
-	usages: "tid",
-	cooldowns: 5, 
-	dependencies: '',
+  name: "tid",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
+  description: "𝙂𝙧𝙪𝙥𝙚𝙧 𝙞𝙙 𝙟𝙖𝙣𝙩𝙚 𝙘𝙝𝙖𝙞",
+  commandCategory: "𝙂𝙧𝙪𝙥",
+  usages: "tid",
+  cooldowns: 5,
+  dependencies: ""
 };
 
 module.exports.run = async function({ api, event }) {
-  api.sendMessage(`𝙀𝙞 𝙜𝙧𝙪𝙥𝙚𝙧 𝙞𝙙: ${event.threadID}`, event.threadID, event.messageID);
+  return api.sendMessage(
+    `𝙀𝙞 𝙜𝙧𝙪𝙥𝙚𝙧 𝙞𝙙: ${event.threadID}`,
+    event.threadID,
+    event.messageID
+  );
 };
