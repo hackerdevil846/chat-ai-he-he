a1a281a218069bc9f2781bd2e2645dee685b4668
kira <kirajanu76@gmail.com>
2025-08-13 21:04:58 +0600

    Update top.js

--- scripts/cmds/top.js
+++ scripts/cmds/top.js
@@ -4,59 +4,60 @@ module.exports.config = {
   hasPermssion: 0,
   credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
   description: "𝑺𝒆𝒓𝒗𝒆𝒓 𝒆𝒓 𝒕𝒐𝒑 𝒄𝒉𝒂𝒓𝒕!",
-  commandCategory: "𝒈𝒓𝒐𝒖𝒑",
+  category: "𝒈𝒓𝒐𝒖𝒑",
   usages: "[𝒕𝒉𝒓𝒆𝒂𝒅/𝒖𝒔𝒆𝒓/𝒎𝒐𝒏𝒆𝒚/𝒍𝒆𝒗𝒆𝒍]",
   cooldowns: 5
 };
 
 module.exports.run = async ({ event, api, args, Currencies, Users }) => {
-    const { threadID, messageID } = event;
+  const { threadID, messageID } = event;
 
-  ///////////////////////////////////////////
-  //===== 𝒍𝒊𝒔𝒕 𝒆𝒓 𝒅𝒐𝒊𝒓𝒈𝒉𝒐 𝒆𝒌𝒕𝒊 𝒔𝒐𝒏𝒌𝒉𝒂 𝒉𝒐𝒕𝒆 𝒉𝒐𝒃𝒆 =====//
-  if (args[1] && isNaN(args[1]) || parseInt(args[1]) <= 0) return api.sendMessage("𝑳𝒊𝒔𝒕 𝒆𝒓 𝒅𝒐𝒊𝒓𝒈𝒉𝒐 𝒆𝒌𝒕𝒊 𝒔𝒐𝒏𝒌𝒉𝒂 𝒉𝒐𝒕𝒆 𝒉𝒐𝒃𝒆 𝒂𝒓 𝒕𝒂 0 𝒕𝒉𝒆𝒌𝒆 𝒃𝒆𝒔𝒊 𝒉𝒐𝒕𝒆 𝒉𝒐𝒃𝒆", event.threadID, event.messageID);
-  var option = parseInt(args[1] || 10);
-  var data, msg = "";
+  // Validate numeric argument if provided
+  if ((args[1] && isNaN(args[1])) || parseInt(args[1]) <= 0) {
+    return api.sendMessage(
+      "𝑳𝒊𝒔𝒕 𝒆𝒓 𝒅𝒐𝒊𝒓𝒈𝒉𝒐 𝒆𝒌𝒕𝒊 𝒔𝒐𝒏𝒌𝒉𝒂 𝒉𝒐𝒕𝒆 𝒉𝒐𝒃𝒆 𝒂𝒓 𝒕𝒂 0 𝒕𝒉𝒆𝒌𝒆 𝒃𝒆𝒔𝒊 𝒉𝒐𝒕𝒆 𝒉𝒐𝒃𝒆",
+      threadID,
+      messageID
+    );
+  }
 
-  ///////////////////////////////////////
-  //===== 𝒌𝒊𝒔 𝒄𝒉𝒊𝒛 𝒆𝒓 𝒕𝒐𝒑 𝒅𝒆𝒌𝒉𝒂𝒃𝒆 =====//
-  var fs = require("fs-extra");
-  var request = require("request");
-  
-  // 𝒆𝒙𝒑 𝒕𝒐 𝒍𝒆𝒗𝒆𝒍 𝒄𝒐𝒏𝒗𝒆𝒓𝒔𝒊𝒐𝒏
+  const option = parseInt(args[1] || 10);
+  let data, msg = "";
+
+  // For level conversion
   function expToLevel(point) {
     if (point < 0) return 0;
     return Math.floor((Math.sqrt(1 + (4 * point) / 3) + 1) / 2);
   }
 
-  // 𝒍𝒆𝒗𝒆𝒍 𝒕𝒐𝒑
-  if (args[0] == "user" || args[0] == "level") { 
-    let all = await Currencies.getAll(['userID', 'exp']);
+  // ========== Top Users by Level ==========
+  if (args[0] === "user" || args[0] === "level") {
+    const all = await Currencies.getAll(["userID", "exp"]);
     all.sort((a, b) => b.exp - a.exp);
+
     let num = 0;
     let topMsg = {
-      body: '𝑺𝒂𝒓𝒃𝒆𝒓 𝒆𝒓 𝒔𝒃𝒐𝒄𝒄𝒉𝒂 𝒖𝒄𝒄𝒉 𝒍𝒆𝒗𝒆𝒍𝒆𝒓 10 𝒋𝒂𝒏:',
-    }
-    for (var i = 0; i < 10; i++) {
+      body: "𝑺𝒂𝒓𝒃𝒆𝒓 𝒆𝒓 𝒔𝒃𝒐𝒄𝒄𝒉𝒂 𝒖𝒄𝒄𝒉 𝒍𝒆𝒗𝒆𝒍𝒆𝒓 10 𝒋𝒂𝒏:"
+    };
+
+    for (let i = 0; i < 10; i++) {
       try {
-        let level = expToLevel(all[i].exp);
-        var userInfo = await Users.getData(all[i].userID);
-        var name = userInfo.name || "𝑨𝒏𝒐𝒏𝒚𝒎𝒐𝒖𝒔";
-        num += 1;
-        topMsg.body += '\n' + num + '. ' + name + ' - 𝒍𝒆𝒗𝒆𝒍 ' + level;
-      } catch (error) {
-        console.error("𝑼𝒔𝒆𝒓 𝒊𝒏𝒇𝒐 𝒑𝒂𝒐𝒘𝒂 𝒋𝒂𝒄𝒄𝒉𝒆 𝒏𝒂: ", error);
+        const level = expToLevel(all[i].exp);
+        const userInfo = await Users.getData(all[i].userID);
+        const name = userInfo.name || "𝑨𝒏𝒐𝒏𝒚𝒎𝒐𝒖𝒔";
+        num++;
+        topMsg.body += `\n${num}. ${name} - 𝒍𝒆𝒗𝒆𝒍 ${level}`;
+      } catch (e) {
+        console.error("𝑼𝒔𝒆𝒓 𝒊𝒏𝒇𝒐 𝒑𝒂𝒐𝒘𝒂 𝒋𝒂𝒄𝒄𝒉𝒆 𝒏𝒂: ", e);
       }
     }
-    api.sendMessage(topMsg, event.threadID, event.messageID);
+
+    return api.sendMessage(topMsg, threadID, messageID);
   }
 
-  // 𝒈𝒓𝒐𝒖𝒑 𝒕𝒐𝒑
-  else if (args[0] == "thread") {
-    var threadList = [];
-    
-    //////////////////////////////////////////////
-    //===== 𝒔𝒐𝒃 𝒈𝒓𝒐𝒖𝒑 𝒂𝒃𝒐𝒏𝒈 𝒎𝒆𝒔𝒔𝒂𝒈𝒆 𝒔𝒂𝒏𝒌𝒉𝒚𝒂 =====//
+  // ========== Top Groups by Message Count ==========
+  else if (args[0] === "thread") {
+    const threadList = [];
     try {
       data = await api.getThreadList(option + 10, null, ["INBOX"]);
     } catch (e) {
@@ -64,63 +65,61 @@ module.exports.run = async ({ event, api, args, Currencies, Users }) => {
       return api.sendMessage("𝑮𝒓𝒐𝒖𝒑 𝒍𝒊𝒔𝒕 𝒑𝒂𝒐𝒘𝒂 𝒋𝒂𝒄𝒄𝒉𝒆 𝒏𝒂", threadID, messageID);
     }
 
-    for (const thread of data) {
-      if (thread.isGroup == true) {
-        threadList.push({ 
-          threadName: thread.name, 
-          threadID: thread.threadID, 
-          messageCount: thread.messageCount 
+    for (const t of data) {
+      if (t.isGroup) {
+        threadList.push({
+          threadName: t.name,
+          threadID: t.threadID,
+          messageCount: t.messageCount
         });
       }
     }
-    
-    /////////////////////////////////////////////////////
-    //===== 𝒔𝒃𝒐𝒄𝒄𝒉𝒂 𝒎𝒆𝒔𝒔𝒂𝒈𝒆 𝒘𝒂𝒍𝒂 𝒈𝒓𝒐𝒖𝒑 𝒔𝒂𝒋𝒂𝒐 =====//
+
     threadList.sort((a, b) => b.messageCount - a.messageCount);
 
-    ///////////////////////////////////////////////////////////////
-    //===== 𝒓𝒆𝒔𝒖𝒍𝒕 𝒔𝒂𝒋𝒂𝒏𝒐 =====//
-    var i = 0;
-    msg = "𝑺𝒂𝒓𝒃𝒐𝒄𝒄𝒉𝒂 𝒎𝒆𝒔𝒔𝒂𝒈𝒆 𝒔𝒐𝒎𝒖𝒅𝒓𝒊 𝒕𝒐𝒑 " + threadList.length + " 𝒈𝒓𝒐𝒖𝒑:\n";
-    for(const dataThread of threadList) {
-      if (i == option) break;
-      msg += `\n${i+1}. ${dataThread.threadName || "𝑵𝒂𝒎 𝒏𝒆𝒊"}\n𝑻𝒉𝒓𝒆𝒂𝒅 𝑰𝑫: ${dataThread.threadID}\n𝑴𝒆𝒔𝒔𝒂𝒈𝒆𝒓 𝒔𝒂𝒏𝒌𝒉𝒚𝒂: ${dataThread.messageCount}\n`;
-      i += 1;
+    msg = `𝑺𝒂𝒓𝒃𝒐𝒄𝒄𝒉𝒂 𝒎𝒆𝒔𝒔𝒂𝒈𝒆 𝒔𝒐𝒎𝒖𝒅𝒓𝒊 𝒕𝒐𝒑 ${threadList.length} 𝒈𝒓𝒐𝒖𝒑:\n`;
+
+    for (let i = 0; i < option && i < threadList.length; i++) {
+      const d = threadList[i];
+      msg += `\n${i + 1}. ${d.threadName || "𝑵𝒂𝒎 𝒏𝒆𝒊"}\nThread ID: ${d.threadID}\nMessages: ${d.messageCount}\n`;
     }
-    
+
     return api.sendMessage(msg, threadID, messageID);
   }
-  
-  // 𝒎𝒐𝒏𝒆𝒚 𝒕𝒐𝒑
-  else if (args[0] == "money") { 
-    let all = await Currencies.getAll(['userID', 'money']);
+
+  // ========== Top Users by Money ==========
+  else if (args[0] === "money") {
+    const all = await Currencies.getAll(["userID", "money"]);
     all.sort((a, b) => b.money - a.money);
+
     let num = 0;
     let topMsg = {
-      body: '𝑺𝒂𝒓𝒃𝒆𝒓 𝒆𝒓 𝒔𝒃𝒐𝒄𝒄𝒉𝒂 𝒅𝒉𝒂𝒏𝒊 10 𝒋𝒂𝒏:',
-    }
-    for (var i = 0; i < 10; i++) {
+      body: "𝑺𝒂𝒓𝒃𝒆𝒓 𝒆𝒓 𝒔𝒃𝒐𝒄𝒄𝒉𝒂 𝒅𝒉𝒂𝒏𝒊 10 𝒋𝒂𝒏:"
+    };
+
+    for (let i = 0; i < 10; i++) {
       try {
-        let money = all[i].money;
-        var userInfo = await Users.getData(all[i].userID);
-        var name = userInfo.name || "𝑨𝒏𝒐𝒏𝒚𝒎𝒐𝒖𝒔";
-        num += 1;
-        topMsg.body += '\n' + num + '. ' + name + ': ' + money + " 💵";
-      } catch (error) {
-        console.error("𝑼𝒔𝒆𝒓 𝒊𝒏𝒇𝒐 𝒑𝒂𝒐𝒘𝒂 𝒋𝒂𝒄𝒄𝒉𝒆 𝒏𝒂: ", error);
+        const money = all[i].money;
+        const userInfo = await Users.getData(all[i].userID);
+        const name = userInfo.name || "𝑨𝒏𝒐𝒏𝒚𝒎𝒐𝒖𝒔";
+        num++;
+        topMsg.body += `\n${num}. ${name}: ${money} 💵`;
+      } catch (e) {
+        console.error("𝑼𝒔𝒆𝒓 𝒊𝒏𝒇𝒐 𝒑𝒂𝒐𝒘𝒂 𝒋𝒂𝒄𝒄𝒉𝒆 𝒏𝒂: ", e);
       }
     }
-    api.sendMessage(topMsg, event.threadID, event.messageID);
+
+    return api.sendMessage(topMsg, threadID, messageID);
   }
 
-  // 𝒆𝒓𝒓𝒐𝒓 𝒉𝒂𝒏𝒅𝒍𝒊𝒏𝒈
+  // ========== Usage Help ==========
   else {
     return api.sendMessage(
-      "𝑼𝒔𝒂𝒈𝒆: 𝒕𝒐𝒑 [𝒕𝒉𝒓𝒆𝒂𝒅/𝒖𝒔𝒆𝒓/𝒎𝒐𝒏𝒆𝒚/𝒍𝒆𝒗𝒆𝒍]\n\n" +
-      "𝑬𝒙𝒂𝒎𝒑𝒍𝒆:\n" +
-      "𝒕𝒐𝒑 𝒕𝒉𝒓𝒆𝒂𝒅 5\n" +
-      "𝒕𝒐𝒑 𝒎𝒐𝒏𝒆𝒚\n" +
-      "𝒕𝒐𝒑 𝒖𝒔𝒆𝒓",
+      "𝑼𝒔𝒂𝒈𝒆: top [thread/user/money/level]\n\n" +
+      "Examples:\n" +
+      "top thread 5\n" +
+      "top money\n" +
+      "top user",
       threadID,
       messageID
     );
