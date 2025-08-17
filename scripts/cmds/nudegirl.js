c483ea2e3ea016860b787f34ee9a3c4835d7cab7
kira <kirajanu76@gmail.com>
2025-08-17 15:17:48 +0600

    Update nudegirl.js

--- scripts/cmds/nudegirl.js
+++ scripts/cmds/nudegirl.js
@@ -13,60 +13,31 @@ module.exports.config = {
   }
 };
 
-module.exports.run = async ({ api, event }) => {
+module.exports.run = async function({ api, event }) {
   const axios = global.nodemodule["axios"];
   const fs = global.nodemodule["fs-extra"];
-  
-  const links = [
-    "https://i.imgur.com/meyGJvz.jpg",
-    "https://i.imgur.com/2n4l5Yq.jpg",
-    "https://i.imgur.com/PxngxOD.jpg",
-    "https://i.imgur.com/A0fbJ6d.jpg",
-    "https://i.imgur.com/sEkcx60.jpg",
-    "https://i.imgur.com/PB2RoCl.jpg",
-    "https://i.imgur.com/Fu4sWId.jpg",
-    "https://i.imgur.com/NrReaFG.jpg",
-    "https://i.imgur.com/CrdN1mS.jpg",
-    "https://i.imgur.com/rGyiCqb.jpg",
-    "https://i.imgur.com/wXZGi7T.jpg",
-    "https://i.imgur.com/Mu92PEc.jpg",
-    "https://i.imgur.com/sZMd93X.jpg",
-    "https://i.imgur.com/1kXE6eJ.jpg",
-    "https://i.imgur.com/CWDtOXZ.jpg",
-    "https://i.imgur.com/s1W3c57.jpg",
-    "https://i.imgur.com/QDlVYDW.jpg",
-    "https://i.imgur.com/6APRg4d.jpg",
-    "https://i.imgur.com/qitBUPy.jpg",
-    "https://i.imgur.com/LwgzvVk.jpg",
-    "https://i.imgur.com/PxVGoau.jpg",
-    "https://i.imgur.com/Tz00ugw.jpg",
-    "https://i.imgur.com/aWStCHt.jpg",
-    "https://i.imgur.com/ERPxzhs.jpg",
-    "https://i.imgur.com/igTYusM.jpg",
-    "https://i.imgur.com/lUsHdL0.jpg",
-    "https://i.imgur.com/P4MwhIi.jpg",
-    "https://i.imgur.com/qP4MZJW.jpg",
-    "https://i.imgur.com/XOQUPus.jpg",
-    "https://i.imgur.com/uRNq4q5.jpg",
-    "https://i.imgur.com/hvhj5Av.jpg",
-    "https://i.imgur.com/19M5A6q.jpg",
-    "https://i.imgur.com/FK16e5v.jpg"
-  ];
+  const path = __dirname + "/cache/nudegirl.jpg";
 
   try {
-    const randomLink = links[Math.floor(Math.random() * links.length)];
-    const imagePath = __dirname + "/cache/nudegirl.jpg";
+    api.sendMessage("🔄 𝒄𝒉𝒐𝒃𝒊 𝒆𝒓 𝒑𝒊𝒄 𝒅𝒐𝒘𝒏𝒍𝒐𝒂𝒅 𝒉𝒐𝒄𝒄𝒉𝒆...", event.threadID);
+
+    const response = await axios.get("https://api.nekosapi.com/v4/images/random?rating=explicit");
+    const imageUrl = response.data.url;
     
-    const response = await axios.get(randomLink, { responseType: "arraybuffer" });
-    fs.writeFileSync(imagePath, Buffer.from(response.data, 'binary'));
+    const imageResponse = await axios.get(imageUrl, { 
+      responseType: "arraybuffer",
+      headers: { "Cache-Control": "no-cache" }
+    });
     
-    api.sendMessage({
-      body: "𝑺𝒂𝒓𝒂𝒅𝒊𝒏 𝒂𝒎𝒊 𝒋𝒂𝒏𝒊 𝑩𝒖𝒔𝒄𝒖 𝑩𝒖𝒔𝒍𝒐𝒏. 😏",
-      attachment: fs.createReadStream(imagePath)
-    }, event.threadID, () => fs.unlinkSync(imagePath), event.messageID);
+    fs.writeFileSync(path, Buffer.from(imageResponse.data, 'binary'));
+    
+    return api.sendMessage({
+      body: `𝑬𝒊 𝑵𝒂𝒐 𝑬𝒙𝒄𝒍𝒖𝒔𝒊𝒗 𝑵𝒖𝒅𝒆 𝑨𝒏𝒊𝒎𝒆 𝑮𝒂𝒓𝒍! 😏\n`,
+      attachment: fs.createReadStream(path)
+    }, event.threadID, () => fs.unlinkSync(path), event.messageID);
     
   } catch (error) {
-    console.error(error);
-    api.sendMessage("❌ 𝒆𝒓𝒓𝒐𝒓: 𝒄𝒉𝒐𝒃𝒊 𝒑𝒂𝒕𝒉𝒂𝒏𝒐 𝒋𝒂𝒕𝒆𝒄𝒉𝒆 𝒏𝒂!", event.threadID, event.messageID);
+    console.error("❌ 𝑵𝒖𝒅𝒆 𝑮𝒊𝒓𝒍 𝑬𝒓𝒓𝒐𝒓:", error);
+    return api.sendMessage("❌ 𝒆𝒓𝒓𝒐𝒓: 𝒄𝒉𝒐𝒃𝒊 𝒑𝒂𝒕𝒉𝒂𝒏𝒐 𝒋𝒂𝒕𝒆𝒄𝒉𝒆 𝒏𝒂! 😢", event.threadID, event.messageID);
   }
 };
