80ff652f5cf6bbe1c807a22e46defde0b7dd2a67
kira <kirajanu76@gmail.com>
2025-08-17 11:31:16 +0600

    Update pair4.js

--- scripts/cmds/pair4.js
+++ scripts/cmds/pair4.js
@@ -3,86 +3,111 @@ module.exports.config = {
 	version: "1.0.1",
 	hasPermssion: 0,
 	credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
-	description: "𝑮𝒓𝒐𝒖𝒑 𝒆𝒓 𝒎𝒆𝒎𝒃𝒆𝒓𝒅𝒆𝒓 𝒔𝒂𝒕𝒉𝒆 𝒑𝒂𝒊𝒓 𝒌𝒐𝒓𝒂",
-	commandCategory: "𝒖𝒔𝒆𝒓𝒅𝒆𝒓 𝒆𝒓 𝒋𝒐𝒏𝒏𝒚𝒐",
+	description: "🍓 𝑮𝒓𝒐𝒖𝒑 𝒎𝒆𝒎𝒃𝒆𝒓𝒔 𝒑𝒂𝒊𝒓𝒊𝒏𝒈 𝒇𝒆𝒂𝒕𝒖𝒓𝒆",
+	commandCategory: "💞 𝒓𝒆𝒍𝒂𝒕𝒊𝒐𝒏𝒔𝒉𝒊𝒑",
 	cooldowns: 5,
+	usages: "[mention/reply/leave blank]",
 	dependencies: {
-        "axios": "",
-        "fs-extra": ""
-  }
-}
+		"axios": "",
+		"fs-extra": "",
+		"jimp": ""
+	},
+	envConfig: {}
+};
 
-module.exports.onLoad = async() => {
-    const { resolve } = global.nodemodule["path"];
-    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
-    const { downloadFile } = global.utils;
-    const dirMaterial = __dirname + `/cache/canvas/`;
-    const path = resolve(__dirname, 'cache/canvas', 'pairing.png');
-    if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.postimg.cc/X7R3CLmb/267378493-3075346446127866-4722502659615516429-n.png", path);
+module.exports.onLoad = async function() {
+	const { resolve } = global.nodemodule["path"];
+	const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+	const { downloadFile } = global.utils;
+	const dirMaterial = __dirname + `/cache/canvas/`;
+	const path = resolve(__dirname, 'cache/canvas', 'pairing.png');
+	
+	if (!existsSync(dirMaterial)) 
+		mkdirSync(dirMaterial, { recursive: true });
+	
+	if (!existsSync(path)) 
+		await downloadFile("https://i.postimg.cc/X7R3CLmb/267378493-3075346446127866-4722502659615516429-n.png", path);
 }
 
 async function makeImage({ one, two }) {
-    const fs = global.nodemodule["fs-extra"];
-    const path = global.nodemodule["path"];
-    const axios = global.nodemodule["axios"]; 
-    const jimp = global.nodemodule["jimp"];
-    const __root = path.resolve(__dirname, "cache", "canvas");
+	const fs = global.nodemodule["fs-extra"];
+	const path = global.nodemodule["path"];
+	const axios = global.nodemodule["axios"]; 
+	const jimp = global.nodemodule["jimp"];
+	const __root = path.resolve(__dirname, "cache", "canvas");
 
-    let pairing_img = await jimp.read(__root + "/pairing.png");
-    let pathImg = __root + `/pairing_${one}_${two}.png`;
-    let avatarOne = __root + `/avt_${one}.png`;
-    let avatarTwo = __root + `/avt_${two}.png`;
-    
-    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
-    
-    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
-    
-    let circleOne = await jimp.read(await circle(avatarOne));
-    let circleTwo = await jimp.read(await circle(avatarTwo));
-    pairing_img.composite(circleOne.resize(150, 150), 980, 200).composite(circleTwo.resize(150, 150), 140, 200);
-    
-    let raw = await pairing_img.getBufferAsync("image/png");
-    
-    fs.writeFileSync(pathImg, raw);
-    fs.unlinkSync(avatarOne);
-    fs.unlinkSync(avatarTwo);
-    
-    return pathImg;
+	let pairing_img = await jimp.read(__root + "/pairing.png");
+	let pathImg = __root + `/pairing_${one}_${two}.png`;
+	let avatarOne = __root + `/avt_${one}.png`;
+	let avatarTwo = __root + `/avt_${two}.png`;
+	
+	let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { 
+		responseType: 'arraybuffer' 
+	})).data;
+	fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
+	
+	let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { 
+		responseType: 'arraybuffer' 
+	})).data;
+	fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
+	
+	let circleOne = await jimp.read(await circle(avatarOne));
+	let circleTwo = await jimp.read(await circle(avatarTwo));
+	pairing_img.composite(circleOne.resize(150, 150), 980, 200)
+			  .composite(circleTwo.resize(150, 150), 140, 200);
+	
+	let raw = await pairing_img.getBufferAsync("image/png");
+	fs.writeFileSync(pathImg, raw);
+	fs.unlinkSync(avatarOne);
+	fs.unlinkSync(avatarTwo);
+	
+	return pathImg;
 }
 
 async function circle(image) {
-    const jimp = require("jimp");
-    image = await jimp.read(image);
-    image.circle();
-    return await image.getBufferAsync("image/png");
+	const jimp = global.nodemodule["jimp"];
+	image = await jimp.read(image);
+	image.circle();
+	return await image.getBufferAsync("image/png");
 }
 
-module.exports.run = async function({ api, event, args, models, Users, Threads, Currencies, permssion }) {
-  const { threadID, messageID, senderID } = event;
-    const { readFileSync, writeFileSync } = require("fs-extra")
-    const fs = require("fs-extra");
-    var tl = ['21%','11%','55%','89%','22%','45%','1%','4%','78%','15%','91%','77%','41%','32%', '67%', '19%', '37%', '17%', '96%', '52%', '62%', '76%', '83%', '100%', '99%', "0%", "48%"];
-        var tle = tl[Math.floor(Math.random() * tl.length)];
-        let dataa = await api.getUserInfo(event.senderID);
-        let namee = await dataa[event.senderID].name
-        let loz = await api.getThreadInfo(event.threadID);
-        var emoji = loz.participantIDs;
-        var id = emoji[Math.floor(Math.random() * emoji.length)];
-        let data = await api.getUserInfo(id);
-        let name = await data[id].name
-        var arraytag = [];
-                arraytag.push({id: event.senderID, tag: namee});
-                arraytag.push({id: id, tag: name});
-        
-        var sex = await data[id].gender;
-        var gender = sex == 2 ? "𝑴𝒂𝒍𝒆🧑" : sex == 1 ? "𝑭𝒆𝒎𝒂𝒍𝒆👩‍🦰" : "𝑻𝒓𝒂𝒏𝒔𝒈𝒆𝒏𝒅𝒆𝒓";
-var one = senderID, two = id;
-    return makeImage({ one, two }).then(path => api.sendMessage({ 
-        body: `🍓 𝑨𝒃𝒉𝒊𝒏𝒂𝒏𝒅𝒂𝒏 ${namee}, 𝒕𝒖𝒎𝒊 𝒑𝒂𝒊𝒓 𝒉𝒐𝒍𝒆 ${name} 𝒆𝒓 𝒔𝒂𝒕𝒉𝒆!\n🍓 𝑻𝒐𝒎𝒂𝒅𝒆𝒓 𝒎𝒊𝒍𝒂𝒏𝒆𝒓 𝒉𝒂𝒓: ${tle}`,
-        mentions: arraytag, 
-        attachment: fs.createReadStream(path) 
-    }, threadID, () => fs.unlinkSync(path), messageID));
+module.exports.run = async function({ api, event }) {
+	const { threadID, messageID, senderID } = event;
+	const fs = global.nodemodule["fs-extra"];
+	
+	// Compatibility percentages
+	const tl = ['21%', '11%', '55%', '89%', '22%', '45%', '1%', '4%', 
+				'78%', '15%', '91%', '77%', '41%', '32%', '67%', '19%', 
+				'37%', '17%', '96%', '52%', '62%', '76%', '83%', '100%', 
+				'99%', "0%", "48%"];
+	const tle = tl[Math.floor(Math.random() * tl.length)];
+	
+	// Get sender info
+	const senderInfo = await api.getUserInfo(senderID);
+	const senderName = senderInfo[senderID].name;
+	
+	// Get random participant
+	const threadInfo = await api.getThreadInfo(threadID);
+	const participant = threadInfo.participantIDs[Math.floor(Math.random() * threadInfo.participantIDs.length)];
+	const participantInfo = await api.getUserInfo(participant);
+	const participantName = participantInfo[participant].name;
+	
+	// Create mention array
+	const arraytag = [
+		{ id: senderID, tag: senderName },
+		{ id: participant, tag: participantName }
+	];
+	
+	// Generate pairing image
+	const path = await makeImage({ 
+		one: senderID, 
+		two: participant 
+	});
+	
+	// Send result
+	return api.sendMessage({ 
+		body: `🌸┈┈┈┈┈┈┈┈┈┈┈┈🌸\n🍓 𝑨𝒃𝒉𝒊𝒏𝒂𝒏𝒅𝒂𝒏 ${senderName}, 𝒕𝒖𝒎𝒊 𝒑𝒂𝒊𝒓 𝒉𝒐𝒍𝒆 ${participantName} 𝒆𝒓 𝒔𝒂𝒕𝒉𝒆!\n💝 𝑻𝒐𝒎𝒂𝒅𝒆𝒓 𝒎𝒊𝒍𝒂𝒏𝒆𝒓 𝒉𝒂𝒓: ${tle}\n🌸┈┈┈┈┈┈┈┈┈┈┈┈🌸`,
+		mentions: arraytag,
+		attachment: fs.createReadStream(path) 
+	}, threadID, () => fs.unlinkSync(path), messageID);
 }
