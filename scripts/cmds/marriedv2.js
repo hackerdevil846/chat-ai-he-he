a923ed02234efdc42c18c3e65ceaaf4b8f49ad03
kira <kirajanu76@gmail.com>
2025-08-19 01:23:45 +0600

    Update marriedv2.js

--- scripts/cmds/marriedv2.js
+++ scripts/cmds/marriedv2.js
@@ -1,11 +1,16 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+const jimp = require("jimp");
+
 module.exports.config = {
     name: "marriedv2",
     version: "3.1.1",
     hasPermssion: 0,
     credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
-    description: "𝑴𝒂𝒓𝒓𝒊𝒆𝒅 𝒊𝒎𝒂𝒈𝒆 𝒄𝒓𝒆𝒂𝒕𝒆 𝒌𝒐𝒓𝒖𝒏",
-    commandCategory: "𝑰𝒎𝒂𝒈𝒆",
-    usages: "[@𝒎𝒆𝒏𝒕𝒊𝒐𝒏]",
+    description: "💍 Married image create korun",
+    commandCategory: "Image",
+    usages: "[@mention]",
     cooldowns: 5,
     dependencies: {
         "axios": "",
@@ -15,69 +20,87 @@ module.exports.config = {
     }
 };
 
-module.exports.onLoad = async() => {
-    const { resolve } = global.nodemodule["path"];
-    const { existsSync, mkdirSync } = global.nodemodule["fs-extra"];
+module.exports.onLoad = async () => {
+    const { resolve } = path;
+    const { existsSync, mkdirSync } = fs;
     const { downloadFile } = global.utils;
     const dirMaterial = __dirname + `/cache/canvas/`;
-    const path = resolve(__dirname, 'cache/canvas', 'marriedv02.png');
-    if (!existsSync(dirMaterial + "canvas")) mkdirSync(dirMaterial, { recursive: true });
-    if (!existsSync(path)) await downloadFile("https://i.ibb.co/mc9KNm1/1619885987-21-pibig-info-p-anime-romantika-svadba-anime-krasivo-24.jpg", path);
+    const filePath = resolve(__dirname, "cache/canvas", "marriedv02.png");
+
+    if (!existsSync(dirMaterial)) mkdirSync(dirMaterial, { recursive: true });
+    if (!existsSync(filePath)) {
+        await downloadFile("https://i.ibb.co/mc9KNm1/1619885987-21-pibig-info-p-anime-romantika-svadba-anime-krasivo-24.jpg", filePath);
+    }
+};
+
+// Circular crop function
+async function circle(image) {
+    const img = await jimp.read(image);
+    img.circle();
+    return await img.getBufferAsync("image/png");
 }
 
+// Make married image
 async function makeImage({ one, two }) {
-    const fs = global.nodemodule["fs-extra"];
-    const path = global.nodemodule["path"];
-    const axios = global.nodemodule["axios"]; 
-    const jimp = global.nodemodule["jimp"];
     const __root = path.resolve(__dirname, "cache", "canvas");
 
     let married_img = await jimp.read(__root + "/marriedv02.png");
     let pathImg = __root + `/married_${one}_${two}.png`;
     let avatarOne = __root + `/avt_${one}.png`;
     let avatarTwo = __root + `/avt_${two}.png`;
-    
-    let getAvatarOne = (await axios.get(`https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, 'utf-8'));
-    
-    let getAvatarTwo = (await axios.get(`https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`, { responseType: 'arraybuffer' })).data;
-    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, 'utf-8'));
-    
+
+    // Get Avatars
+    let getAvatarOne = (await axios.get(
+        `https://graph.facebook.com/${one}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
+        { responseType: "arraybuffer" }
+    )).data;
+    fs.writeFileSync(avatarOne, Buffer.from(getAvatarOne, "utf-8"));
+
+    let getAvatarTwo = (await axios.get(
+        `https://graph.facebook.com/${two}/picture?width=512&height=512&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`,
+        { responseType: "arraybuffer" }
+    )).data;
+    fs.writeFileSync(avatarTwo, Buffer.from(getAvatarTwo, "utf-8"));
+
+    // Make circular & composite
     let circleOne = await jimp.read(await circle(avatarOne));
     let circleTwo = await jimp.read(await circle(avatarTwo));
-    married_img.composite(circleOne.resize(100, 100), 55, 48).composite(circleTwo.resize(100, 100), 190, 40);
-    
+    married_img
+        .composite(circleOne.resize(100, 100), 55, 48)
+        .composite(circleTwo.resize(100, 100), 190, 40);
+
     let raw = await married_img.getBufferAsync("image/png");
-    
+
     fs.writeFileSync(pathImg, raw);
     fs.unlinkSync(avatarOne);
     fs.unlinkSync(avatarTwo);
-    
+
     return pathImg;
 }
 
-async function circle(image) {
-    const jimp = require("jimp");
-    image = await jimp.read(image);
-    image.circle();
-    return await image.getBufferAsync("image/png");
-}
-
-module.exports.run = async function ({ event, api, args }) {    
-    const fs = global.nodemodule["fs-extra"];
+module.exports.run = async function ({ event, api }) {
     const { threadID, messageID, senderID } = event;
     const mention = Object.keys(event.mentions);
-    
+
     if (!mention[0]) {
-        return api.sendMessage("𝑫𝒂𝒚𝒂 𝒌𝒐𝒓𝒆 𝒆𝒌𝒋𝒐𝒏 𝒌𝒆 𝒎𝒆𝒏𝒕𝒊𝒐𝒏 𝒌𝒐𝒓𝒖𝒏! 💍", threadID, messageID);
-    }
-    else {
-        const one = senderID, two = mention[0];
-        return makeImage({ one, two }).then(path => {
-            api.sendMessage({ 
-                body: `💕 𝑪𝒐𝒏𝒈𝒓𝒂𝒕𝒖𝒍𝒂𝒕𝒊𝒐𝒏𝒔! 𝑺𝒉𝒂𝒅𝒊 𝒆𝒓 𝒊𝒎𝒂𝒈𝒆 𝒓𝒆𝒂𝒅𝒚 💒\n━━━━━━━━━━━━━━\n𝑷𝒐𝒘𝒆𝒓𝒆𝒅 𝒃𝒚 𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅`,
-                attachment: fs.createReadStream(path) 
-            }, threadID, () => fs.unlinkSync(path), messageID);
+        return api.sendMessage(
+            "💍 | 𝑷𝒍𝒆𝒂𝒔𝒆 𝒎𝒆𝒏𝒕𝒊𝒐𝒏 𝒂𝒏𝒐𝒕𝒉𝒆𝒓 𝒖𝒔𝒆𝒓 𝒕𝒐 𝒎𝒂𝒓𝒓𝒚!",
+            threadID,
+            messageID
+        );
+    } else {
+        const one = senderID,
+            two = mention[0];
+        return makeImage({ one, two }).then(pathImg => {
+            api.sendMessage(
+                {
+                    body: "💕 | 𝑪𝒐𝒏𝒈𝒓𝒂𝒕𝒖𝒍𝒂𝒕𝒊𝒐𝒏𝒔! 𝑴𝒂𝒓𝒓𝒊𝒂𝒈𝒆 𝒄𝒆𝒓𝒕𝒊𝒇𝒊𝒄𝒂𝒕𝒆 𝒄𝒓𝒆𝒂𝒕𝒆𝒅!\n━━━━━━━━━━━━━━\n𝑷𝒐𝒘𝒆𝒓𝒆𝒅 𝒃𝒚 𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
+                    attachment: fs.createReadStream(pathImg)
+                },
+                threadID,
+                () => fs.unlinkSync(pathImg),
+                messageID
+            );
         });
     }
-}
+};
