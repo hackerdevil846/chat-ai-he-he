2dbd0242413f6ac15e47e80ab89ff6b7e655231a
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-24 02:27:45 +0600

    Update love3.js

--- scripts/cmds/love3.js
+++ scripts/cmds/love3.js
@@ -9,7 +9,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
 	description: "দুইজন ইউজারের romantic love image তৈরি করবে 💖",
-	commandCategory: "edit-img",
+	category: "edit-img",
 	usages: "love3 @mention",
 	cooldowns: 5,
 	dependencies: {
