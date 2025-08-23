9dd22352f027a6046c7da1568dfc630e9c71b960
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-24 02:34:59 +0600

    Update marriedv2.js

--- scripts/cmds/marriedv2.js
+++ scripts/cmds/marriedv2.js
@@ -9,7 +9,7 @@ module.exports.config = {
     hasPermssion: 0,
     credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
     description: "💍 Married image create korun",
-    commandCategory: "Image",
+    category: "Image",
     usages: "[@mention]",
     cooldowns: 5,
     dependencies: {
