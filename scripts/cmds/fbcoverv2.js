2020a5e411853b393614fab2de22a1cec95a1868
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-23 23:47:50 +0600

    Update fbcoverv2.js

--- scripts/cmds/fbcoverv2.js
+++ scripts/cmds/fbcoverv2.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
 	description: "Facebook cover creation tool",
-	commandCategory: "image",
+	category: "image",
 	usages: "",
 	cooldowns: 5,
 	dependencies: {
