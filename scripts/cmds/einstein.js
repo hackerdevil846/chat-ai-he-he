0647789eca1d995597cb6d9b8df3bf70d5aec79f
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-23 23:08:54 +0600

    Update einstein.js

--- scripts/cmds/einstein.js
+++ scripts/cmds/einstein.js
@@ -4,7 +4,7 @@ module.exports.config = {
 	hasPermssion: 0,
 	credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
 	description: "🖋️ 𝑩𝒐𝒂𝒓𝒅 𝒆 𝒄𝒐𝒎𝒎𝒆𝒏𝒕 𝒌𝒐𝒓𝒖𝒏",
-	commandCategory: "🖼️ 𝑴𝒆𝒎𝒆𝒔",
+	category: "🖼️ 𝑴𝒆𝒎𝒆𝒔",
 	usages: "[𝒕𝒆𝒙𝒕]",
 	cooldowns: 5,
 	dependencies: {
