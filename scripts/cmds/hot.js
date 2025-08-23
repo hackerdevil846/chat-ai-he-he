b3d8054556a72335afc5005cfb65c1c394d2a046
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-24 01:27:51 +0600

    Update hot.js

--- scripts/cmds/hot.js
+++ scripts/cmds/hot.js
@@ -25,7 +25,7 @@ module.exports.config = {
   hasPermssion: 0,
   credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
   description: "Get a random hot video",
-  commandCategory: "media",
+  category: "media",
   usages: "hot",
   cooldowns: 5,
   dependencies: {
