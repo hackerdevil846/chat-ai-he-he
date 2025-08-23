aa087fa156c8fe5df4279f64ae451d58e25d6032
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-24 00:58:42 +0600

    Update gf.js

--- scripts/cmds/gf.js
+++ scripts/cmds/gf.js
@@ -4,7 +4,7 @@ module.exports.config = {
     hasPermssion: 0,
     credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
     description: "𝑴𝒆𝒏𝒕𝒊𝒐𝒏 𝒌𝒐𝒓𝒆 𝒑𝒂𝒊𝒓 𝒑𝒂𝒃𝒐 💞",
-    commandCategory: "𝗜𝗠𝗔𝗚𝗘",
+    category: "𝗜𝗠𝗔𝗚𝗘",
     usages: "[@𝒎𝒆𝒏𝒕𝒊𝒐𝒏]",
     cooldowns: 5,
     dependencies: {
