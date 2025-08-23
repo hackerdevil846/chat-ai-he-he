4000f65b3e40558a68ed3ccd3a246d41f3f1cf0f
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-23 20:19:07 +0600

    Update checktt.js

--- scripts/cmds/checktt.js
+++ scripts/cmds/checktt.js
@@ -4,7 +4,7 @@ module.exports.config = {
     hasPermssion: 0,
     credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
     description: "Interactive message counter & rank checker",
-    commandCategory: "Utilities",
+    category: "Utilities",
     usages: "[all/rank/@mention]",
     cooldowns: 5,
     dependencies: {
