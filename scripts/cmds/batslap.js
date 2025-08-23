5773960fdb9802f238169614f9c8ceecf3facefc
kira <kirajanu76@gmail.com>
2025-08-23 15:05:12 +0600

    Update batslap.js

--- scripts/cmds/batslap.js
+++ scripts/cmds/batslap.js
@@ -1,44 +1,56 @@
 const DIG = require("discord-image-generation");
 const fs = require("fs-extra");
 
-module.exports = {
-	config: {
-		name: "batslap",
-		version: "1.1",
-		author: "Asif",
-		countDown: 5,
-		role: 0,
-		shortDescription: "Batslap image",
-		longDescription: "Batslap image",
-		category: "image",
-		guide: {
-			en: "   {pn} @tag"
-		}
-	},
+module.exports.config = {
+    name: "batslap",
+    version: "1.1",
+    hasPermssion: 0,
+    credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
+    description: "🎭 Batslap image generator",
+    commandCategory: "random-img",
+    usages: "@tag [message]",
+    cooldowns: 5,
+    dependencies: {
+        "discord-image-generation": "",
+        "fs-extra": ""
+    },
+    envConfig: {}
+};
 
-	langs: {
-		vi: {
-			noTag: "Bạn phải tag người bạn muốn tát"
-		},
-		en: {
-			noTag: "You must tag the person you want to slap"
-		}
-	},
+module.exports.languages = {
+    "vi": {
+        noTag: "Bạn phải tag người bạn muốn tát"
+    },
+    "en": {
+        noTag: "❌ You must tag the person you want to slap!"
+    }
+};
 
-	onStart: async function ({ event, message, usersData, args, getLang }) {
-		const uid1 = event.senderID;
-		const uid2 = Object.keys(event.mentions)[0];
-		if (!uid2)
-			return message.reply(getLang("noTag"));
-		const avatarURL1 = await usersData.getAvatarUrl(uid1);
-		const avatarURL2 = await usersData.getAvatarUrl(uid2);
-		const img = await new DIG.Batslap().getImage(avatarURL1, avatarURL2);
-		const pathSave = `${__dirname}/tmp/${uid1}_${uid2}Batslap.png`;
-		fs.writeFileSync(pathSave, Buffer.from(img));
-		const content = args.join(' ').replace(Object.keys(event.mentions)[0], "");
-		message.reply({
-			body: `${(content || "Bópppp 😵‍💫😵")}`,
-			attachment: fs.createReadStream(pathSave)
-		}, () => fs.unlinkSync(pathSave));
-	}
+module.exports.onLoad = function() {
+    // Runs when command is loaded
+};
+
+module.exports.run = async function({ api, event, args, Users }) {
+    try {
+        const uid1 = event.senderID;
+        const uid2 = Object.keys(event.mentions)[0];
+        if (!uid2) return api.sendMessage(global.GoatBot.getLang("en", "noTag"), event.threadID);
+
+        const avatarURL1 = await Users.getAvatarUrl(uid1);
+        const avatarURL2 = await Users.getAvatarUrl(uid2);
+
+        const img = await new DIG.Batslap().getImage(avatarURL1, avatarURL2);
+        const pathSave = `${__dirname}/tmp/${uid1}_${uid2}Batslap.png`;
+        fs.writeFileSync(pathSave, Buffer.from(img));
+
+        const content = args.join(' ').replace(Object.keys(event.mentions)[0], "") || "Bópppp 😵‍💫😵";
+        api.sendMessage({
+            body: `💥 ${content}`,
+            attachment: fs.createReadStream(pathSave)
+        }, event.threadID, () => fs.unlinkSync(pathSave));
+
+    } catch (error) {
+        console.error(error);
+        api.sendMessage("❌ Something went wrong while generating the Batslap image!", event.threadID);
+    }
 };
