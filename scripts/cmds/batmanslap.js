bdd7eeffaeb4adfc28fd7ae77c653e4ddfcf008b
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-23 19:29:40 +0600

    Update batmanslap.js

--- scripts/cmds/batmanslap.js
+++ scripts/cmds/batmanslap.js
@@ -4,7 +4,7 @@ module.exports.config = {
     hasPermssion: 0,
     credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
     description: "🦇 Batslap meme creator",
-    commandCategory: "general",
+    category: "general",
     usages: "[tag]",
     cooldowns: 5,
     dependencies: {
