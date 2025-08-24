46e989f0b22731711845f5e0373ed5756e651eb1
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-25 01:03:12 +0600

    Update antirobbery.js

--- scripts/cmds/antirobbery.js
+++ scripts/cmds/antirobbery.js
@@ -10,23 +10,36 @@ module.exports.config = {
 };
 
 module.exports.run = async ({ api, event, Threads }) => {
-    const info = await api.getThreadInfo(event.threadID);
-    if (!info.adminIDs.some(item => item.id == api.getCurrentUserID())) 
-      return api.sendMessage('❌ 𝑵𝒆𝒆𝒅 𝒈𝒓𝒐𝒖𝒑 𝒂𝒅𝒎𝒊𝒏𝒊𝒔𝒕𝒓𝒂𝒕𝒐𝒓 𝒑𝒆𝒓𝒎𝒊𝒔𝒔𝒊𝒐𝒏𝒔, 𝒑𝒍𝒆𝒂𝒔𝒆 𝒂𝒅𝒅 𝒃𝒐𝒕 𝒂𝒔 𝒂𝒅𝒎𝒊𝒏 𝒂𝒏𝒅 𝒕𝒓𝒚 𝒂𝒈𝒂𝒊𝒏!', event.threadID, event.messageID);
-    
-    const data = (await Threads.getData(event.threadID)).data || {};
-    if (typeof data["guard"] == "undefined" || data["guard"] == false) {
-        data["guard"] = true;
-    } else {
-        data["guard"] = false;
-    }
-    
-    await Threads.setData(event.threadID, { data });
-    global.data.threadData.set(parseInt(event.threadID), data);
-    
+  const info = await api.getThreadInfo(event.threadID);
+  if (!info.adminIDs.some(item => item.id == api.getCurrentUserID())) 
     return api.sendMessage(
-        `✅ ${(data["guard"] === true) ? "𝑨𝒏𝒕𝒊-𝑹𝒐𝒃𝒃𝒆𝒓𝒚 𝒔𝒚𝒔𝒕𝒆𝒎 𝒂𝒄𝒕𝒊𝒗𝒂𝒕𝒆𝒅" : "𝑨𝒏𝒕𝒊-𝑹𝒐𝒃𝒃𝒆𝒓𝒚 𝒔𝒚𝒔𝒕𝒆𝒎 𝒅𝒆𝒂𝒄𝒕𝒊𝒗𝒂𝒕𝒆𝒅"}`,
-        event.threadID,
-        event.messageID
+      '❌ 𝑵𝒆𝒆𝒅 𝒈𝒓𝒐𝒖𝒑 𝒂𝒅𝒎𝒊𝒏𝒊𝒔𝒕𝒓𝒂𝒕𝒐𝒓 𝒑𝒆𝒓𝒎𝒊𝒔𝒔𝒊𝒐𝒏𝒔, 𝒑𝒍𝒆𝒂𝒔𝒆 𝒂𝒅𝒅 𝒃𝒐𝒕 𝒂𝒔 𝒂𝒅𝒎𝒊𝒏 𝒂𝒏𝒅 𝒕𝒓𝒚 𝒂𝒈𝒂𝒊𝒏!', 
+      event.threadID, 
+      event.messageID
     );
+  
+  const data = (await Threads.getData(event.threadID)).data || {};
+  if (typeof data["guard"] == "undefined" || data["guard"] == false) {
+      data["guard"] = true;
+  } else {
+      data["guard"] = false;
+  }
+  
+  await Threads.setData(event.threadID, { data });
+  global.data.threadData.set(parseInt(event.threadID), data);
+  
+  return api.sendMessage(
+      `✅ ${(data["guard"] === true) ? "𝑨𝒏𝒕𝒊-𝑹𝒐𝒃𝒃𝒆𝒓𝒚 𝒔𝒚𝒔𝒕𝒆𝒎 𝒂𝒄𝒕𝒊𝒗𝒂𝒕𝒆𝒅" : "𝑨𝒏𝒕𝒊-𝑹𝒐𝒃𝒃𝒆𝒓𝒚 𝒔𝒚𝒔𝒕𝒆𝒎 𝒅𝒆𝒂𝒄𝒕𝒊𝒗𝒂𝒕𝒆𝒅"}`,
+      event.threadID,
+      event.messageID
+  );
+};
+
+// onStart function to prevent the "undefined" error
+module.exports.onStart = async ({ Threads }) => {
+  const allThreadData = await Threads.getAll();
+  for (const thread of allThreadData) {
+    if (!thread.data.guard) thread.data.guard = false;
+    global.data.threadData.set(parseInt(thread.threadID), thread.data);
+  }
 };
