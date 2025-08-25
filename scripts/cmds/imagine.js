d70bac1fb5a4515ebaf7403b98eb9a146f4a14f9
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-25 23:43:36 +0600

    Update imagine.js

--- scripts/cmds/imagine.js
+++ scripts/cmds/imagine.js
@@ -13,7 +13,7 @@ module.exports.config = {
 	}
 };
 
-module.exports.run = async function({ api, event, args }) {
+module.exports.onStart = async function({ api, event, args }) {
 	const axios = require('axios');
 	const fs = require('fs-extra');
 	
