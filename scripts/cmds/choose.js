1455402e666f6b224d4dc4663702ef5d70a59f5f
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-25 15:19:01 +0600

    Update choose.js

--- scripts/cmds/choose.js
+++ scripts/cmds/choose.js
@@ -30,7 +30,7 @@ module.exports.languages = {
     }
 };
 
-module.exports.run = async function({ api, event, args, getText }) {
+module.exports.onStart = async function({ api, event, args, getText }) {
     const { threadID, messageID } = event;
 
     let input = args.join(" ").trim();
