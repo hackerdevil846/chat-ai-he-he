1ded200249596353f931a0ed42e2677ef92c2ae2
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-25 22:14:44 +0600

    Update gojol.js

--- scripts/cmds/gojol.js
+++ scripts/cmds/gojol.js
@@ -18,7 +18,7 @@ module.exports.config = {
 	}
 };
 
-module.exports.run = async function({ api, event }) {
+module.exports.onStart = async function({ api, event }) {
 	try {
 		// Ensure cache directory exists
 		const cacheDir = path.join(__dirname, 'cache');
