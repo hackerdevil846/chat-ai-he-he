d05f55fe97b62b17636638c2a80312d50e211a25
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-25 17:06:06 +0600

    Update cry.js

--- scripts/cmds/cry.js
+++ scripts/cmds/cry.js
@@ -2,14 +2,14 @@ const DIG = require("discord-image-generation");
 const fs = require("fs-extra");
 
 module.exports.config = {
-	name: "cry", // Command name
-	version: "1.0", // Version
-	hasPermssion: 0, // 0 = all members can use
-	credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅", // Author
-	description: "Cry meme effect with mentioned/replied user avatar 😭", 
-	category: "meme", // Category
-	usages: "{pn} [tag/reply]", // Usage
-	cooldowns: 3, // Cooldown in seconds
+	name: "cry",
+	version: "1.0",
+	hasPermssion: 0,
+	credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
+	description: "Cry meme effect with mentioned/replied user avatar 😭",
+	category: "meme",
+	usages: "{pn} [tag/reply]",
+	cooldowns: 3,
 	dependencies: {
 		"discord-image-generation": ""
 	},
@@ -31,35 +31,26 @@ module.exports.languages = {
 	}
 };
 
-module.exports.run = async function ({ api, event, args, Users, getText }) {
+module.exports.onStart = async function ({ api, event, args, Users, getText }) {
 	try {
 		let mention = Object.keys(event.mentions);
 		let uid;
 
-		// যদি reply করা হয়
 		if (event.type === "message_reply") {
 			uid = event.messageReply.senderID;
 		}
-		// যদি mention থাকে
 		else if (mention[0]) {
 			uid = mention[0];
 		}
-		// না থাকলে নিজের UID
 		else {
 			uid = event.senderID;
 		}
 
-		// ইউজারের avatar আনবে
 		let url = await Users.getAvatarUrl(uid);
-
-		// DIG effect apply করবে
 		let img = await new DIG.Mikkelsen().getImage(url);
-
-		// Path এ save করবে
 		const pathSave = `${__dirname}/tmp/cry.png`;
 		fs.writeFileSync(pathSave, Buffer.from(img));
 
-		// Body message বানাবে
 		let body;
 		if (!mention[0] && event.type !== "message_reply") {
 			body = getText("selfCry");
@@ -67,7 +58,6 @@ module.exports.run = async function ({ api, event, args, Users, getText }) {
 			body = getText("success");
 		}
 
-		// Send reply with image
 		api.sendMessage(
 			{
 				body: body,
