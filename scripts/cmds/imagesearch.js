1ecee3f785140dc7b4754c99c965106dc64c540b
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-25 23:42:41 +0600

    Update imagesearch.js

--- scripts/cmds/imagesearch.js
+++ scripts/cmds/imagesearch.js
@@ -18,7 +18,7 @@ module.exports.config = {
   }
 };
 
-module.exports.run = async function({ api, event, args }) {
+module.exports.onStart = async function({ api, event, args }) {
   const axios = global.nodemodule['axios'];
   const google = global.nodemodule["googlethis"];
   const cloudscraper = global.nodemodule["cloudscraper"];
