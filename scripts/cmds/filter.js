4c6793aba4eb49bf4e307c7c6c4fccd535d3bf96
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-25 20:54:34 +0600

    Update filter.js

--- scripts/cmds/filter.js
+++ scripts/cmds/filter.js
@@ -11,7 +11,7 @@ module.exports.config = {
     envConfig: {}
 };
 
-module.exports.run = async function({ api, event, args }) {
+module.exports.onStart = async function({ api, event, args }) {
     try {
         // Get thread information
         const threadInfo = await api.getThreadInfo(event.threadID);
