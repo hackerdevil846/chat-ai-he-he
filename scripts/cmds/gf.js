5191402dc826b6094e4deb9e719dd1ac3d674898
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-25 21:42:00 +0600

    Update gf.js

--- scripts/cmds/gf.js
+++ scripts/cmds/gf.js
@@ -26,7 +26,7 @@ module.exports.onLoad = async function() {
     if (!existsSync(path)) await downloadFile("https://i.imgur.com/iaOiAXe.jpeg", path);
 }
 
-module.exports.run = async function({ event, api, args }) {
+module.exports.onStart = async function({ event, api, args }) {
     const fs = global.nodemodule["fs-extra"];
     const path = global.nodemodule["path"];
     const axios = global.nodemodule["axios"];
