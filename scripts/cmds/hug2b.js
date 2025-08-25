219b3554f3a1182120eec48d9530f361142a966b
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-25 23:29:21 +0600

    Update hug2b.js

--- scripts/cmds/hug2b.js
+++ scripts/cmds/hug2b.js
@@ -26,7 +26,7 @@ module.exports.onLoad = async function() {
     if (!existsSync(path)) await downloadFile("https://i.ibb.co/3YN3T1r/q1y28eqblsr21.jpg", path);
 }
 
-module.exports.run = async function({ event, api, args }) {
+module.exports.onStart = async function({ event, api, args }) {
     const fs = global.nodemodule["fs-extra"];
     const axios = global.nodemodule["axios"];
     const jimp = global.nodemodule["jimp"];
