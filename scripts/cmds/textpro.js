eb8b57fe1da344d4c74c97a173401155f999243c
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-26 20:17:04 +0600

    Update textpro.js

--- scripts/cmds/textpro.js
+++ scripts/cmds/textpro.js
@@ -16,7 +16,7 @@ module.exports.config = {
     }
 };
 
-module.exports.run = async function ({ api, event, args }) {
+module.exports.onStart = async function ({ api, event, args }) {
     const { threadID, messageID } = event;
 
     // Check if user provided text
