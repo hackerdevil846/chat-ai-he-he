82be90a25d7a091f8b690374d2d659c02d50f5ae
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-26 15:32:25 +0600

    Update nsfw2.js

--- scripts/cmds/nsfw2.js
+++ scripts/cmds/nsfw2.js
@@ -12,7 +12,7 @@ module.exports.config = {
 	}
 };
 
-module.exports.run = async function({ api, event, args }) {
+module.exports.onStart = async function({ api, event, args }) {
 	const { threadID, messageID } = event;
 	const axios = require("axios");
 	
