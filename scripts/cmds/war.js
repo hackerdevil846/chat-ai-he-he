056af5a4aae1133112a069b1b57a56ece95d4a35
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-26 20:51:50 +0600

    Update war.js

--- scripts/cmds/war.js
+++ scripts/cmds/war.js
@@ -13,7 +13,7 @@ module.exports.config = {
   }
 };
 
-module.exports.run = async function({ api, event, args, Users }) {
+module.exports.onStart = async function({ api, event, args, Users }) {
   try {
     const { threadID, mentions } = event;
 
