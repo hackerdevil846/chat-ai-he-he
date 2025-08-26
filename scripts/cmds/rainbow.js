0348457fecc4259c1b9a6b8f192ecb68d0a0d7e1
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-26 15:52:48 +0600

    Update rainbow.js

--- scripts/cmds/rainbow.js
+++ scripts/cmds/rainbow.js
@@ -16,7 +16,7 @@ module.exports.config = {
   }
 };
 
-module.exports.run = async function({ api, event, args }) {
+module.exports.onStart = async function({ api, event, args }) {
   const { threadID, messageID } = event;
   const value = parseInt(args[0]);
   
