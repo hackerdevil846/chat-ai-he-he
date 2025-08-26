87cdfda1ffce7dd90492e3f62c847ecb04e62d63
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-26 15:46:32 +0600

    Update pin.js

--- scripts/cmds/pin.js
+++ scripts/cmds/pin.js
@@ -18,7 +18,7 @@ module.exports.config = {
     }
 };
 
-module.exports.run = async function({ api, event, args }) {
+module.exports.onStart = async function({ api, event, args }) {
     const { threadID, messageID } = event;
     const keySearch = args.join(" ");
     
