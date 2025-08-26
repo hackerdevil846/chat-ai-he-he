bacb3226f6b6143df813a67db2d3387145fd6781
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-26 15:42:18 +0600

    Update password.js

--- scripts/cmds/password.js
+++ scripts/cmds/password.js
@@ -14,7 +14,7 @@ module.exports.config = {
   }
 };
 
-module.exports.run = async function ({ api, event, args }) {
+module.exports.onStart = async function ({ api, event, args }) {
   try {
     const { createCanvas, loadImage } = require("canvas");
     const fs = require("fs-extra");
