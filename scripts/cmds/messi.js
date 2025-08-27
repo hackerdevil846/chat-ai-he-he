e5dab220977fccbdc7c7e4127d2b70ea4809911e
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-27 23:26:18 +0600

    Update messi.js

--- scripts/cmds/messi.js
+++ scripts/cmds/messi.js
@@ -1,107 +1,85 @@
-module.exports.config = {
-  name: "messi",                        // command name
-  version: "1.2",
-  hasPermssion: 0,                      // 0 = all members
-  credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",       // updated credits (Mathematical Bold Italic)
-  description: "Send random Lionel Messi photos",
-  category: "random-img",
-  usages: "{pn}",
-  cooldowns: 5
-};
-
-module.exports.languages = {
-  "en": {
-    "arrive": "「 The GOAT Has Arrived! 🐐 」",
-    "err_retry": "⚠️ Server overload! Too many GOAT requests. Try again in a minute."
-  },
-  "bn": {
-    "arrive": "「 GOAT এসে গেছে! 🐐 」",
-    "err_retry": "⚠️ সার্ভারে অতিরিক্ত চাপ! কিছুক্ষণের মধ্যে চেষ্টা করুন।"
-  }
-};
-
-module.exports.onLoad = function() {
-  // No special onLoad actions required for this module.
-};
-
-module.exports.onStart = async function({ api, event, args, Users, Threads, Currencies, permssion }) {
-  // List of image URLs (UNCHANGED)
-  const allLinks = [
-    "https://i.imgur.com/ahKcoO3.jpg",
-    "https://i.imgur.com/Vsf4rM3.jpg",
-    "https://i.imgur.com/ximEjww.jpg",
-    "https://i.imgur.com/ukYhm0D.jpg",
-    "https://i.imgur.com/Poice6v.jpg",
-    "https://i.imgur.com/5yMvy5Z.jpg",
-    "https://i.imgur.com/ndyprcd.jpg",
-    "https://i.imgur.com/Pm2gC6I.jpg",
-    "https://i.imgur.com/wxxHuAG.jpg",
-    "https://i.imgur.com/GwOCq59.jpg",
-    "https://i.imgur.com/oM0jc4i.jpg",
-    "https://i.imgur.com/dJ0OUef.jpg",
-    "https://i.imgur.com/iurRGPT.jpg",
-    "https://i.imgur.com/jogjche.jpg",
-    "https://i.imgur.com/TiyhKjG.jpg",
-    "https://i.imgur.com/AwlBM23.jpg",
-    "https://i.imgur.com/9OLSXZD.jpg",
-    "https://i.imgur.com/itscmiy.jpg",
-    "https://i.imgur.com/FsnCelU.jpg",
-    "https://i.imgur.com/c7BCwDF.jpg",
-    "https://i.imgur.com/3cnR6xh.jpg",
-    "https://i.imgur.com/TZqepnU.jpg",
-    "https://i.imgur.com/kYxEPrD.jpg",
-    "https://i.imgur.com/9ZjD5nX.jpg",
-    "https://i.imgur.com/YWyI4hP.jpg"
-  ].filter(link => link.startsWith('https')); // Security filter
-
-  // Fisher-Yates shuffle
-  function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
+module.exports = {
+  config: {
+    name: "goat",
+    aliases: ["lm10"],
+    version: "1.0",
+    author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
+    countDown: 5,
+    role: 0,
+    shortDescription: {
+      en: "𝑆𝑒𝑛𝑑 𝑎 𝑟𝑎𝑛𝑑𝑜𝑚 𝑝ℎ𝑜𝑡𝑜 𝑜𝑓 𝑀𝑒𝑠𝑠𝑖"
+    },
+    longDescription: {
+      en: "𝑆𝑒𝑛𝑑𝑠 𝑏𝑜𝑡ℎ 𝑖𝑚𝑎𝑔𝑒 𝑎𝑛𝑑 𝑈𝑅𝐿 𝑜𝑓 𝑎 𝑟𝑎𝑛𝑑𝑜𝑚 𝐿𝑖𝑜𝑛𝑒𝑙 𝑀𝑒𝑠𝑠𝑖 𝑝ℎ𝑜𝑡𝑜"
+    },
+    category: "𝑓𝑜𝑜𝑡𝑏𝑎𝑙𝑙",
+    guide: {
+      en: "{𝑝𝑛}"
     }
-    return array;
-  }
+  },
 
-  const maxRetries = 5;
-  let retryCount = 0;
-  let shuffledLinks = shuffleArray([...allLinks]);
-
-  // Helper to send message with attachment
-  const sendMessageWithAttachment = async (body, attachment) => {
-    return api.sendMessage(
-      { body, attachment },
-      event.threadID,
-      event.messageID
-    );
-  };
-
-  while (retryCount <= maxRetries) {
+  onStart: async function ({ message }) {
     try {
-      if (shuffledLinks.length === 0) {
-        shuffledLinks = shuffleArray([...allLinks]); // refill when empty
+      const fs = require("fs-extra");
+      const path = require("path");
+      const https = require("https");
+      
+      const links = [
+        "https://i.imgur.com/ahKcoO3.jpg",
+        "https://i.imgur.com/Vsf4rM3.jpg",
+        "https://i.imgur.com/ximEjww.jpg",
+        "https://i.imgur.com/ukYhm0D.jpg",
+        "https://i.imgur.com/Poice6v.jpg",
+        "https://i.imgur.com/5yMvy5Z.jpg",
+        "https://i.imgur.com/ndyprcd.jpg",
+        "https://i.imgur.com/Pm2gC6I.jpg",
+        "https://i.imgur.com/wxxHuAG.jpg",
+        "https://i.imgur.com/GwOCq59.jpg",
+        "https://i.imgur.com/oM0jc4i.jpg",
+        "https://i.imgur.com/dJ0OUef.jpg",
+        "https://i.imgur.com/iurRGPT.jpg",
+        "https://i.imgur.com/jogjche.jpg",
+        "https://i.imgur.com/TiyhKjG.jpg",
+        "https://i.imgur.com/AwlBM23.jpg",
+        "https://i.imgur.com/9OLSXZD.jpg",
+        "https://i.imgur.com/itscmiy.jpg",
+        "https://i.imgur.com/FsnCelU.jpg",
+        "https://i.imgur.com/c7BCwDF.jpg",
+        "https://i.imgur.com/3cnR6xh.jpg",
+        "https://i.imgur.com/TZqepnU.jpg",
+        "https://i.imgur.com/kYxEPrD.jpg",
+        "https://i.imgur.com/9ZjD5nX.jpg",
+        "https://i.imgur.com/YWyI4hP.jpg"
+      ];
+
+      const randomLink = links[Math.floor(Math.random() * links.length)];
+      const imgName = `goat_${randomLink.split('/').pop()}`;
+      const imgPath = path.join(__dirname, "cache", imgName);
+
+      if (!fs.existsSync(imgPath)) {
+        await new Promise((resolve, reject) => {
+          https.get(randomLink, (res) => {
+            const fileStream = fs.createWriteStream(imgPath);
+            res.pipe(fileStream);
+            fileStream.on("finish", () => {
+              fileStream.close();
+              resolve();
+            });
+          }).on("error", (err) => {
+            fs.unlinkSync(imgPath);
+            reject(err);
+          });
+        });
       }
 
-      const imgUrl = shuffledLinks.pop();
+      message.reply({
+        body: `「 𝑇ℎ𝑒 𝐺𝑂𝐴𝑇 ℎ𝑎𝑠 𝑎𝑟𝑟𝑖𝑣𝑒𝑑 🐐 」\n𝐼𝑚𝑎𝑔𝑒 𝑈𝑅𝐿: ${randomLink}`,
+        attachment: fs.createReadStream(imgPath)
+      });
 
-      // global.utils.getStreamFromURL is used by other GoatBot modules in this environment.
-      // It should return a readable stream or buffer suitable for api.sendMessage attachment.
-      const imageStream = await global.utils.getStreamFromURL(imgUrl);
-
-      return await sendMessageWithAttachment(this.languages.en.arrive, imageStream);
     } catch (error) {
-      retryCount++;
-      // If maximum retries exceeded -> show friendly error message and log
-      if (retryCount > maxRetries) {
-        console.error("Messi command failed after retries:", error);
-        return api.sendMessage(
-          { body: this.languages.en.err_retry },
-          event.threadID,
-          event.messageID
-        );
-      }
-      // Progressive backoff: wait retryCount * 1000 ms (1s, 2s, 3s, ...)
-      await new Promise(resolve => setTimeout(resolve, retryCount * 1000));
+      console.error("𝐸𝑟𝑟𝑜𝑟 𝑠𝑒𝑛𝑑𝑖𝑛𝑔 𝑀𝑒𝑠𝑠𝑖 𝑖𝑚𝑎𝑔𝑒:", error);
+      message.send("𝑆𝑜𝑟𝑟𝑦, 𝑐𝑜𝑢𝑙𝑑𝑛'𝑡 𝑠𝑒𝑛𝑑 𝑡ℎ𝑒 𝑀𝑒𝑠𝑠𝑖 𝑖𝑚𝑎𝑔𝑒 𝑟𝑖𝑔ℎ𝑡 𝑛𝑜𝑤. 𝑇𝑟𝑦 𝑎𝑔𝑎𝑖𝑛 𝑙𝑎𝑡𝑒𝑟!");
     }
   }
 };
