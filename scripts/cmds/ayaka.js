14fea66a1a18385486c22d59fb4f44ff00203f2f
hackerdevil846 <hackerdevil846@gmail.com>
2025-08-28 22:32:21 +0600

    Update ayaka.js

--- scripts/cmds/ayaka.js
+++ scripts/cmds/ayaka.js
@@ -1,143 +1,156 @@
-module.exports.config = {
-  name: "ayaka",
-  version: "1.0.0",
-  hasPermssion: 0,
-  credits: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
-  description: "𝑨𝒚𝒂𝒌𝒂 𝒆𝒓 𝒓𝒂𝒏𝒅𝒐𝒎 𝒊𝒎𝒂𝒈𝒆",
-  category: "𝑹𝒂𝒏𝒅𝒐𝒎-𝑰𝑴𝑮",
-  usages: "ayaka",
-  cooldowns: 5,
-  dependencies: {
-    "request":"",
-    "fs-extra":"",
-    "axios":""
-  }
-};
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
 
-module.exports.onStart = async ({ api, event, args, Users, Threads, Currencies }) => {
-  const axios = global.nodemodule["axios"];
-  const request = global.nodemodule["request"];
-  const fs = global.nodemodule["fs-extra"];
-  
-  var link = [
-    "https://i.imgur.com/uXWLBeC.jpeg",
-    "https://i.imgur.com/7Dc9GrN.jpeg",
-    "https://i.imgur.com/IaAVMFK.jpeg",
-    "https://i.imgur.com/WceNH2z.jpeg",
-    "https://i.imgur.com/1XosaEA.jpeg",
-    "https://i.imgur.com/M58fVe6.jpeg",
-    "https://i.imgur.com/czaXZ3a.jpeg",
-    "https://i.imgur.com/xsu6v2I.jpeg",
-    "https://i.imgur.com/f17dCCM.jpeg",
-    "https://i.imgur.com/opquSuU.jpeg",
-    "https://i.imgur.com/U87kL1B.jpeg",
-    "https://i.imgur.com/Osa1EEd.jpeg",
-    "https://i.imgur.com/38XTSUn.jpeg",
-    "https://i.imgur.com/B7mAsZB.jpeg",
-    "https://i.imgur.com/2APmfRs.jpeg",
-    "https://i.imgur.com/mCUOJ8U.jpeg",
-    "https://i.imgur.com/CnN1DxG.jpeg",
-    "https://i.imgur.com/onlEme6.jpeg",
-    "https://i.imgur.com/OF73muW.jpeg",
-    "https://i.imgur.com/UO1sK8I.jpeg",
-    "https://i.imgur.com/AlkGMJr.jpeg",
-    "https://i.imgur.com/yZy8yvG.jpeg",
-    "https://i.imgur.com/wLuwsWH.jpeg",
-    "https://i.imgur.com/NoLgneL.jpeg",
-    "https://i.imgur.com/wnXPqVv.jpeg",
-    "https://i.imgur.com/D4ORkkM.jpeg",
-    "https://i.imgur.com/bXZCoXT.jpeg",
-    "https://i.imgur.com/ixx7Psr.jpeg",
-    "https://i.imgur.com/TWP438b.jpeg",
-    "https://i.imgur.com/zEiGsZE.jpeg",
-    "https://i.imgur.com/pFbFkvj.jpeg",
-    "https://i.imgur.com/U9fPLgz.jpeg",
-    "https://i.imgur.com/VjOIoAg.jpeg",
-    "https://i.imgur.com/gmYkkFF.jpeg",
-    "https://i.imgur.com/4o5MRal.jpeg",
-    "https://i.imgur.com/XDGkXfZ.jpeg",
-    "https://i.imgur.com/B50Pi6m.jpeg",
-    "https://i.imgur.com/BZKVLfn.jpeg",
-    "https://i.imgur.com/wSQv7mM.jpeg",
-    "https://i.imgur.com/2Ky8mww.jpeg",
-    "https://i.imgur.com/4fhxxts.jpeg",
-    "https://i.imgur.com/rvFm33m.jpeg",
-    "https://i.imgur.com/J2EG5QV.jpeg",
-    "https://i.imgur.com/JwkXNeQ.jpeg",
-    "https://i.imgur.com/S9AGlH6.jpeg",
-    "https://i.imgur.com/L9Jg1pg.jpeg",
-    "https://i.imgur.com/urJBEyk.jpeg",
-    "https://i.imgur.com/Hpw0D8O.jpeg",
-    "https://i.imgur.com/i5hdv5w.jpeg",
-    "https://i.imgur.com/O2uymjw.jpeg",
-    "https://i.imgur.com/GiSKHaT.jpeg",
-    "https://i.imgur.com/dAs2g30.jpeg",
-    "https://i.imgur.com/RIhBJhH.jpeg",
-    "https://i.imgur.com/pvSpSEb.jpeg",
-    "https://i.imgur.com/XUJdz0T.jpeg",
-    "https://i.imgur.com/jad2M8w.jpeg",
-    "https://i.imgur.com/vbOsMtC.jpeg",
-    "https://i.imgur.com/ZTtxhm8.jpeg",
-    "https://i.imgur.com/8Qf8hLj.jpeg",
-    "https://i.imgur.com/FXGMlHp.jpeg",
-    "https://i.imgur.com/jWDw41w.jpeg",
-    "https://i.imgur.com/LgvUCju.jpeg",
-    "https://i.imgur.com/sdBRGt3.jpeg",
-    "https://i.imgur.com/I32E7mo.jpeg",
-    "https://i.imgur.com/OBbsiOY.jpeg",
-    "https://i.imgur.com/ZlwE7gK.jpeg",
-    "https://i.imgur.com/RjTJEia.jpeg",
-    "https://i.imgur.com/mihSwWi.jpeg",
-    "https://i.imgur.com/XLLJjEM.jpeg",
-    "https://i.imgur.com/NkMNc9U.jpeg",
-    "https://i.imgur.com/DscSpW9.jpeg",
-    "https://i.imgur.com/jA1JB8Z.jpeg",
-    "https://i.imgur.com/4744YOK.jpeg",
-    "https://i.imgur.com/L7ZmAdP.jpeg",
-    "https://i.imgur.com/fnqGUzZ.jpeg",
-    "https://i.imgur.com/4r5vG6y.jpeg",
-    "https://i.imgur.com/mOZyIBN.jpeg",
-    "https://i.imgur.com/5nKPTdH.jpeg",
-    "https://i.imgur.com/2DoiyZg.jpeg",
-    "https://i.imgur.com/BDvYK5e.jpeg",
-    "https://i.imgur.com/JImr4HA.jpeg",
-    "https://i.imgur.com/SDYcTdB.jpeg",
-    "https://i.imgur.com/GH3rmiF.jpeg",
-    "https://i.imgur.com/tUjsJk6.jpeg",
-    "https://i.imgur.com/jvjWcZ9.jpeg",
-    "https://i.imgur.com/9l5tHki.jpeg",
-    "https://i.imgur.com/P4GYTjs.jpeg",
-    "https://i.imgur.com/4qXII5h.jpeg",
-    "https://i.imgur.com/wix18FM.jpeg",
-    "https://i.imgur.com/h6JyuUd.jpeg",
-    "https://i.imgur.com/agZEIfN.jpeg",
-    "https://i.imgur.com/qQJmQ7X.jpeg",
-    "https://i.imgur.com/SJ7tHsd.jpeg",
-    "https://i.imgur.com/IWsuHJN.jpeg",
-    "https://i.imgur.com/PshaE6A.jpeg",
-    "https://i.imgur.com/OvAjaUQ.jpeg",
-    "https://i.imgur.com/CW4Id3o.jpeg",
-    "https://i.imgur.com/5SWTCJ4.jpeg"
-  ];
-  
-  var max = Math.floor(Math.random() * 6);  
-  var min = Math.floor(Math.random() * 2);
-  var data = await Currencies.getData(event.senderID);
-  var exp = data.exp;
-  var money = data.money;
-  
-  if (money < 100) {
-    api.sendMessage("❌ 𝑨𝒑𝒏𝒂𝒓 𝒌𝒂𝒄𝒉𝒆 𝟏𝟎𝟎 $ 𝒏𝒆𝒊, 𝑨𝒚𝒂𝒌𝒂 𝒆𝒓 𝒑𝒉𝒐𝒕𝒐 𝒅𝒆𝒌𝒉𝒕𝒆 𝒑𝒂𝒓𝒃𝒆𝒏 𝒏𝒂!", event.threadID, event.messageID);
-  } else {
-    Currencies.setData(event.senderID, { money: money - 100 });
-    
-    var callback = () => api.sendMessage({
-      body: `✨ 𝑨𝒚𝒂𝒌𝒂 𝒆𝒓 𝒓𝒂𝒏𝒅𝒐𝒎 𝒊𝒎𝒂𝒈𝒆...\n𝑻𝒐𝒕𝒂𝒍 𝒊𝒎𝒂𝒈𝒆: ${link.length}\n𝑷𝒓𝒊𝒄𝒆: -𝟏𝟎𝟎 $`,
-      attachment: fs.createReadStream(__dirname + "/cache/1.jpg")
-    }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/1.jpg"), event.messageID); 
-    
-    return request(encodeURI(link[Math.floor(Math.random() * link.length)] + (max - min)))
-      .pipe(fs.createWriteStream(__dirname + "/cache/1.jpg"))
-      .on("close", () => callback());
+module.exports = {
+  config: {
+    name: "ayaka",
+    version: "1.0.0",
+    author: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
+    role: 0,
+    category: "random-img",
+    shortDescription: {
+      en: "𝑨𝒚𝒂𝒌𝒂 𝒆𝒓 𝒓𝒂𝒏𝒅𝒐𝒎 𝒊𝒎𝒂𝒈𝒆"
+    },
+    longDescription: {
+      en: "𝑺𝒆𝒏𝒅𝒔 𝒓𝒂𝒏𝒅𝒐𝒎 𝑨𝒚𝒂𝒌𝒂 𝒊𝒎𝒂𝒈𝒆𝒔"
+    },
+    guide: {
+      en: "{p}ayaka"
+    },
+    cooldowns: 5
+  },
+
+  onStart: async function({ message, event, Currencies }) {
+    try {
+      const link = [
+        "https://i.imgur.com/uXWLBeC.jpeg",
+        "https://i.imgur.com/7Dc9GrN.jpeg",
+        "https://i.imgur.com/IaAVMFK.jpeg",
+        "https://i.imgur.com/WceNH2z.jpeg",
+        "https://i.imgur.com/1XosaEA.jpeg",
+        "https://i.imgur.com/M58fVe6.jpeg",
+        "https://i.imgur.com/czaXZ3a.jpeg",
+        "https://i.imgur.com/xsu6v2I.jpeg",
+        "https://i.imgur.com/f17dCCM.jpeg",
+        "https://i.imgur.com/opquSuU.jpeg",
+        "https://i.imgur.com/U87kL1B.jpeg",
+        "https://i.imgur.com/Osa1EEd.jpeg",
+        "https://i.imgur.com/38XTSUn.jpeg",
+        "https://i.imgur.com/B7mAsZB.jpeg",
+        "https://i.imgur.com/2APmfRs.jpeg",
+        "https://i.imgur.com/mCUOJ8U.jpeg",
+        "https://i.imgur.com/CnN1DxG.jpeg",
+        "https://i.imgur.com/onlEme6.jpeg",
+        "https://i.imgur.com/OF73muW.jpeg",
+        "https://i.imgur.com/UO1sK8I.jpeg",
+        "https://i.imgur.com/AlkGMJr.jpeg",
+        "https://i.imgur.com/yZy8yvG.jpeg",
+        "https://i.imgur.com/wLuwsWH.jpeg",
+        "https://i.imgur.com/NoLgneL.jpeg",
+        "https://i.imgur.com/wnXPqVv.jpeg",
+        "https://i.imgur.com/D4ORkkM.jpeg",
+        "https://i.imgur.com/bXZCoXT.jpeg",
+        "https://i.imgur.com/ixx7Psr.jpeg",
+        "https://i.imgur.com/TWP438b.jpeg",
+        "https://i.imgur.com/zEiGsZE.jpeg",
+        "https://i.imgur.com/pFbFkvj.jpeg",
+        "https://i.imgur.com/U9fPLgz.jpeg",
+        "https://i.imgur.com/VjOIoAg.jpeg",
+        "https://i.imgur.com/gmYkkFF.jpeg",
+        "https://i.imgur.com/4o5MRal.jpeg",
+        "https://i.imgur.com/XDGkXfZ.jpeg",
+        "https://i.imgur.com/B50Pi6m.jpeg",
+        "https://i.imgur.com/BZKVLfn.jpeg",
+        "https://i.imgur.com/wSQv7mM.jpeg",
+        "https://i.imgur.com/2Ky8mww.jpeg",
+        "https://i.imgur.com/4fhxxts.jpeg",
+        "https://i.imgur.com/rvFm33m.jpeg",
+        "https://i.imgur.com/J2EG5QV.jpeg",
+        "https://i.imgur.com/JwkXNeQ.jpeg",
+        "https://i.imgur.com/S9AGlH6.jpeg",
+        "https://i.imgur.com/L9Jg1pg.jpeg",
+        "https://i.imgur.com/urJBEyk.jpeg",
+        "https://i.imgur.com/Hpw0D8O.jpeg",
+        "https://i.imgur.com/i5hdv5w.jpeg",
+        "https://i.imgur.com/O2uymjw.jpeg",
+        "https://i.imgur.com/GiSKHaT.jpeg",
+        "https://i.imgur.com/dAs2g30.jpeg",
+        "https://i.imgur.com/RIhBJhH.jpeg",
+        "https://i.imgur.com/pvSpSEb.jpeg",
+        "https://i.imgur.com/XUJdz0T.jpeg",
+        "https://i.imgur.com/jad2M8w.jpeg",
+        "https://i.imgur.com/vbOsMtC.jpeg",
+        "https://i.imgur.com/ZTtxhm8.jpeg",
+        "https://i.imgur.com/8Qf8hLj.jpeg",
+        "https://i.imgur.com/FXGMlHp.jpeg",
+        "https://i.imgur.com/jWDw41w.jpeg",
+        "https://i.imgur.com/LgvUCju.jpeg",
+        "https://i.imgur.com/sdBRGt3.jpeg",
+        "https://i.imgur.com/I32E7mo.jpeg",
+        "https://i.imgur.com/OBbsiOY.jpeg",
+        "https://i.imgur.com/ZlwE7gK.jpeg",
+        "https://i.imgur.com/RjTJEia.jpeg",
+        "https://i.imgur.com/mihSwWi.jpeg",
+        "https://i.imgur.com/XLLJjEM.jpeg",
+        "https://i.imgur.com/NkMNc9U.jpeg",
+        "https://i.imgur.com/DscSpW9.jpeg",
+        "https://i.imgur.com/jA1JB8Z.jpeg",
+        "https://i.imgur.com/4744YOK.jpeg",
+        "https://i.imgur.com/L7ZmAdP.jpeg",
+        "https://i.imgur.com/fnqGUzZ.jpeg",
+        "https://i.imgur.com/4r5vG6y.jpeg",
+        "https://i.imgur.com/mOZyIBN.jpeg",
+        "https://i.imgur.com/5nKPTdH.jpeg",
+        "https://i.imgur.com/2DoiyZg.jpeg",
+        "https://i.imgur.com/BDvYK5e.jpeg",
+        "https://i.imgur.com/JImr4HA.jpeg",
+        "https://i.imgur.com/SDYcTdB.jpeg",
+        "https://i.imgur.com/GH3rmiF.jpeg",
+        "https://i.imgur.com/tUjsJk6.jpeg",
+        "https://i.imgur.com/jvjWcZ9.jpeg",
+        "https://i.imgur.com/9l5tHki.jpeg",
+        "https://i.imgur.com/P4GYTjs.jpeg",
+        "https://i.imgur.com/4qXII5h.jpeg",
+        "https://i.imgur.com/wix18FM.jpeg",
+        "https://i.imgur.com/h6JyuUd.jpeg",
+        "https://i.imgur.com/agZEIfN.jpeg",
+        "https://i.imgur.com/qQJmQ7X.jpeg",
+        "https://i.imgur.com/SJ7tHsd.jpeg",
+        "https://i.imgur.com/IWsuHJN.jpeg",
+        "https://i.imgur.com/PshaE6A.jpeg",
+        "https://i.imgur.com/OvAjaUQ.jpeg",
+        "https://i.imgur.com/CW4Id3o.jpeg",
+        "https://i.imgur.com/5SWTCJ4.jpeg"
+      ];
+
+      // Create cache directory if it doesn't exist
+      const cacheDir = path.join(__dirname, 'cache');
+      if (!fs.existsSync(cacheDir)) {
+        fs.mkdirSync(cacheDir, { recursive: true });
+      }
+
+      const imagePath = path.join(cacheDir, 'ayaka.jpg');
+      const randomImage = link[Math.floor(Math.random() * link.length)];
+
+      // Download the image
+      const response = await axios.get(randomImage, {
+        responseType: 'arraybuffer'
+      });
+
+      fs.writeFileSync(imagePath, Buffer.from(response.data));
+
+      await message.reply({
+        body: `✨ 𝑨𝒚𝒂𝒌𝒂 𝒆𝒓 𝒓𝒂𝒏𝒅𝒐𝒎 𝒊𝒎𝒂𝒈𝒆...\n𝑻𝒐𝒕𝒂𝒍 𝒊𝒎𝒂𝒈𝒆𝒔: ${link.length}`,
+        attachment: fs.createReadStream(imagePath)
+      });
+
+      // Clean up
+      fs.unlinkSync(imagePath);
+
+    } catch (error) {
+      console.error("Ayaka command error:", error);
+      await message.reply("❌ 𝑭𝒂𝒊𝒍𝒆𝒅 𝒕𝒐 𝒍𝒐𝒂𝒅 𝑨𝒚𝒂𝒌𝒂 𝒊𝒎𝒂𝒈𝒆. 𝑷𝒍𝒆𝒂𝒔𝒆 𝒕𝒓𝒚 𝒂𝒈𝒂𝒊𝒏 𝒍𝒂𝒕𝒆𝒓.");
+    }
   }
 };
