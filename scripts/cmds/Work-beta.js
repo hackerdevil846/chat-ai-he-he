1ba3fa469ff1c3b6e65fab129ad4d0e07398a997
hackerdevil846 <hackerdevil846@gmail.com>
2025-09-09 02:29:13 +0600

    Update Work-beta.js

--- scripts/cmds/Work-beta.js
+++ scripts/cmds/Work-beta.js
@@ -1,178 +1,192 @@
 module.exports = {
   config: {
     name: "job",
+    aliases: ["work", "kaj"],
     version: "1.0.2",
-    author: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
+    author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
+    countDown: 5,
     role: 0,
-    category: "economy",
+    category: "𝑒𝑐𝑜𝑛𝑜𝑚𝑦",
     shortDescription: {
-      en: "𝑬𝒂𝒓𝒏 𝒄𝒐𝒊𝒏𝒔 𝒃𝒚 𝒘𝒐𝒓𝒌𝒊𝒏𝒈"
+      en: "𝐸𝑎𝑟𝑛 𝑐𝑜𝑖𝑛𝑠 𝑏𝑦 𝑤𝑜𝑟𝑘𝑖𝑛𝑔"
     },
     longDescription: {
-      en: "𝑾𝒐𝒓𝒌 𝒕𝒐 𝒆𝒂𝒓𝒏 𝒎𝒐𝒏𝒆𝒚 𝒊𝒏 𝒕𝒉𝒆 𝒆𝒄𝒐𝒏𝒐𝒎𝒚 𝒔𝒚𝒔𝒕𝒆𝒎"
+      en: "𝑊𝑜𝑟𝑘 𝑡𝑜 𝑒𝑎𝑟𝑛 𝑚𝑜𝑛𝑒𝑦 𝑖𝑛 𝑡ℎ𝑒 𝑒𝑐𝑜𝑛𝑜𝑚𝑦 𝑠𝑦𝑠𝑡𝑒𝑚"
     },
     guide: {
       en: "{p}job"
-    },
-    cooldowns: 5
+    }
   },
 
   langs: {
     en: {
-      cooldown: "𝑻𝒖𝒎𝒊 𝒌𝒂𝒋 𝒔𝒉𝒆𝒔𝒉 𝒌𝒐𝒓𝒆𝒄𝒉𝒉𝒐, 𝒂𝒃𝒂𝒓 𝒂𝒔𝒉𝒐: %1 𝒎𝒊𝒏𝒖𝒕𝒆(𝒔) %2 𝒔𝒆𝒄𝒐𝒏𝒅(𝒔)."
+      cooldown: "𝑌𝑜𝑢 ℎ𝑎𝑣𝑒 𝑎𝑙𝑟𝑒𝑎𝑑𝑦 𝑤𝑜𝑟𝑘𝑒𝑑, 𝑐𝑜𝑚𝑒 𝑏𝑎𝑐𝑘 𝑖𝑛: %1 𝑚𝑖𝑛𝑢𝑡𝑒(𝑠) %2 𝑠𝑒𝑐𝑜𝑛𝑑(𝑠)."
     }
   },
 
-  onStart: async function({ event, message, Currencies, getLang }) {
-    const cooldownTime = 5 * 60 * 1000; // 5 minutes cooldown
-    let data = (await Currencies.getData(event.senderID)).data || {};
-    
-    if (data.work2Time && (Date.now() - data.work2Time) < cooldownTime) {
-      const timeLeft = cooldownTime - (Date.now() - data.work2Time);
-      const minutes = Math.floor(timeLeft / 60000);
-      const seconds = Math.floor((timeLeft % 60000) / 1000);
+  onStart: async function({ event, message, usersData, getLang }) {
+    try {
+      const cooldownTime = 5 * 60 * 1000; // 5 minutes cooldown
+      const userData = await usersData.get(event.senderID);
+      const userJobData = userData.data || {};
       
-      return message.reply(getLang("cooldown", minutes, seconds));
-    }
-
-    const menu = `𝑪𝒐𝒊𝒏 𝑬𝒂𝒓𝒏 𝑱𝒐𝒃 𝑪𝒆𝒏𝒕𝒆𝒓
-
-1. 𝑰𝒏𝒅𝒖𝒔𝒕𝒓𝒊𝒂𝒍 𝒛𝒐𝒏𝒆 𝒌𝒂𝒋
-2. 𝑺𝒆𝒓𝒗𝒊𝒄𝒆 𝒂𝒓𝒆𝒂 𝒌𝒂𝒋
-3. 𝑶𝒊𝒍 𝒇𝒊𝒆𝒍𝒅 𝒌𝒂𝒋
-4. 𝑴𝒊𝒏𝒊𝒏𝒈 𝒌𝒂𝒋
-5. 𝑫𝒊𝒈𝒈𝒊𝒏𝒈 𝒌𝒂𝒋
-6. 𝑺𝒑𝒆𝒄𝒊𝒂𝒍 𝒋𝒐𝒃
-7. 𝑼𝒑𝒅𝒂𝒕𝒆 𝒔𝒐𝒐𝒏...
-
-⚡️𝑫𝒐𝒚𝒂 𝒌𝒐𝒓𝒆 𝒓𝒆𝒑𝒍𝒚 𝒌𝒐𝒓𝒆 𝒏𝒖𝒎𝒃𝒆𝒓 𝒄𝒉𝒐𝒐𝒔𝒆 𝒌𝒐𝒓𝒖𝒏`;
-
-    await message.reply(menu);
-    
-    // Store the cooldown time
-    data.work2Time = Date.now();
-    await Currencies.setData(event.senderID, { data });
-  },
-
-  onChat: async function({ event, message, Currencies, reply }) {
-    if (event.type === "message_reply") {
-      const userData = (await Currencies.getData(event.senderID)).data || {};
-      
-      if (!userData.work2Time) return;
-      
-      // Job arrays with Bengali translations
-      const rdcn = [
-        '𝒔𝒕𝒂𝒇𝒇 𝒉𝒊𝒓𝒆 𝒌𝒐𝒓𝒕𝒆𝒄𝒉𝒊',
-        '𝒉𝒐𝒕𝒆𝒍 𝒂𝒅𝒎𝒊𝒏𝒊𝒔𝒕𝒓𝒂𝒕𝒐𝒓',
-        '𝒑𝒐𝒘𝒆𝒓 𝒑𝒍𝒂𝒏𝒕 𝒂 𝒌𝒂𝒋 𝒌𝒐𝒓𝒕𝒆𝒄𝒉𝒊',
-        '𝒓𝒆𝒔𝒕𝒂𝒖𝒓𝒂𝒏𝒕 𝒄𝒉𝒆𝒇',
-        '𝒘𝒐𝒓𝒌𝒆𝒓'
-      ];
-      
-      const rddv = [
-        '𝒑𝒍𝒖𝒎𝒃𝒆𝒓',
-        '𝒏𝒆𝒊𝒈𝒉𝒃𝒐𝒓 𝒆𝒓 𝑨𝑪 𝒓𝒆𝒑𝒂𝒊𝒓',
-        '𝒎𝒖𝒍𝒕𝒊-𝒍𝒆𝒗𝒆𝒍 𝒔𝒂𝒍𝒆 𝒌𝒐𝒓𝒕𝒆𝒄𝒉𝒊',
-        '𝒇𝒍𝒚𝒆𝒓 𝒅𝒊𝒔𝒕𝒓𝒊𝒃𝒖𝒕𝒊𝒐𝒏 𝒌𝒐𝒓𝒕𝒆𝒄𝒉𝒊',
-        '𝒔𝒉𝒊𝒑𝒑𝒆𝒓',
-        '𝒄𝒐𝒎𝒑𝒖𝒕𝒆𝒓 𝒓𝒆𝒑𝒂𝒊𝒓 𝒌𝒐𝒓𝒕𝒆𝒄𝒉𝒊',
-        '𝒕𝒐𝒖𝒓 𝒈𝒖𝒊𝒅𝒆',
-        '𝒃𝒖𝒂 𝒆𝒓 𝒌𝒂𝒋'
-      ];
-      
-      const rdmd = [
-        '13 𝒃𝒂𝒓𝒓𝒆𝒍 𝒐𝒊𝒍 𝒆𝒂𝒓𝒏 𝒌𝒐𝒓𝒆𝒄𝒉𝒉𝒊',
-        '8 𝒃𝒂𝒓𝒓𝒆𝒍 𝒐𝒊𝒍 𝒆𝒂𝒓𝒏 𝒌𝒐𝒓𝒆𝒄𝒉𝒉𝒊',
-        '9 𝒃𝒂𝒓𝒓𝒆𝒍 𝒐𝒊𝒍 𝒆𝒂𝒓𝒏 𝒌𝒐𝒓𝒆𝒄𝒉𝒉𝒊',
-        '𝒐𝒊𝒍 𝒄𝒉𝒖𝒓𝒊 𝒌𝒐𝒓𝒕𝒆𝒄𝒉𝒊',
-        '𝒐𝒊𝒍 𝒆 𝒑𝒂𝒏𝒊 𝒎𝒊𝒍𝒊𝒚𝒆 𝒔𝒆𝒍𝒍 𝒌𝒐𝒓𝒆𝒄𝒉𝒉𝒊'
-      ];
-      
-      const rdq = [
-        '𝒊𝒓𝒐𝒏 𝒐𝒓𝒆',
-        '𝒈𝒐𝒍𝒅 𝒐𝒓𝒆',
-        '𝒄𝒐𝒂𝒍 𝒐𝒓𝒆',
-        '𝒍𝒆𝒂𝒅 𝒐𝒓𝒆',
-        '𝒄𝒐𝒑𝒑𝒆𝒓 𝒐𝒓𝒆',
-        '𝒐𝒊𝒍 𝒐𝒓𝒆'
-      ];
-      
-      const rddd = [
-        '𝒅𝒊𝒂𝒎𝒐𝒏𝒅',
-        '𝒈𝒐𝒍𝒅',
-        '𝒄𝒐𝒂𝒍',
-        '𝒆𝒎𝒆𝒓𝒂𝒍𝒅',
-        '𝒊𝒓𝒐𝒏',
-        '𝒐𝒓𝒅𝒊𝒏𝒂𝒓𝒚 𝒔𝒕𝒐𝒏𝒆',
-        '𝒍𝒂𝒛𝒚',
-        '𝒃𝒍𝒖𝒆𝒔𝒕𝒐𝒏𝒆'
-      ];
-      
-      const rddd1 = [
-        '𝒗𝒊𝒑 𝒂𝒕𝒊𝒕𝒉𝒊',
-        '𝒑𝒂𝒕𝒆𝒏𝒕',
-        '𝒐𝒔𝒕𝒓𝒊𝒄𝒉',
-        '23 𝒃𝒐𝒄𝒉𝒐𝒓𝒆𝒓 𝒇𝒐𝒐𝒍',
-        '𝒑𝒂𝒕𝒓𝒐𝒏',
-        '92 𝒃𝒐𝒄𝒉𝒐𝒓𝒆𝒓 𝒕𝒚𝒄𝒐𝒐𝒏',
-        '12 𝒃𝒐𝒄𝒉𝒐𝒓𝒆𝒓 𝒃𝒐𝒚𝒊'
-      ];
-
-      const work1 = rdcn[Math.floor(Math.random() * rdcn.length)];
-      const work2 = rddv[Math.floor(Math.random() * rddv.length)];
-      const work3 = rdmd[Math.floor(Math.random() * rdmd.length)];
-      const work4 = rdq[Math.floor(Math.random() * rdq.length)];
-      const work5 = rddd[Math.floor(Math.random() * rddd.length)];
-      const work6 = rddd1[Math.floor(Math.random() * rddd1.length)];
-
-      // Random coin amounts
-      const coinscn = Math.floor(Math.random() * 401) + 200;
-      const coinsdv = Math.floor(Math.random() * 801) + 200;
-      const coinsmd = Math.floor(Math.random() * 401) + 200;
-      const coinsq = Math.floor(Math.random() * 601) + 200;
-      const coinsdd = Math.floor(Math.random() * 201) + 200;
-      const coinsdd1 = Math.floor(Math.random() * 801) + 200;
-
-      let msg = "";
-      let coinsEarned = 0;
-
-      switch(event.body) {
-        case "1": 
-          msg = `⚡️𝑻𝒖𝒎𝒊 𝒊𝒏𝒅𝒖𝒔𝒕𝒓𝒊𝒂𝒍 𝒛𝒐𝒏𝒆 𝒆 ${work1} 𝒌𝒂𝒋 𝒌𝒐𝒓𝒆 ${coinscn}$ 𝒆𝒂𝒓𝒏 𝒌𝒐𝒓𝒄𝒉𝒐.`;
-          coinsEarned = coinscn;
-          break;
-        case "2": 
-          msg = `⚡️𝑻𝒖𝒎𝒊 𝒔𝒆𝒓𝒗𝒊𝒄𝒆 𝒂𝒓𝒆𝒂 𝒕𝒆 ${work2} 𝒌𝒂𝒋 𝒌𝒐𝒓𝒆 ${coinsdv}$ 𝒆𝒂𝒓𝒏 𝒌𝒐𝒓𝒄𝒉𝒐.`;
-          coinsEarned = coinsdv;
-          break;
-        case "3": 
-          msg = `⚡️𝑻𝒖𝒎𝒊 𝒐𝒑𝒆𝒏 𝒐𝒊𝒍 𝒆 ${work3} 𝒌𝒐𝒓𝒆 ${coinsmd}$ 𝒆𝒂𝒓𝒏 𝒌𝒐𝒓𝒄𝒉𝒐.`;
-          coinsEarned = coinsmd;
-          break;
-        case "4": 
-          msg = `⚡️𝑻𝒖𝒎𝒊 ${work4} 𝒎𝒊𝒏𝒆 𝒌𝒐𝒓𝒆 ${coinsq}$ 𝒆𝒂𝒓𝒏 𝒌𝒐𝒓𝒄𝒉𝒐.`;
-          coinsEarned = coinsq;
-          break;
-        case "5": 
-          msg = `⚡️𝑻𝒖𝒎𝒊 ${work5} 𝒅𝒊𝒈 𝒌𝒐𝒓𝒆 ${coinsdd}$ 𝒆𝒂𝒓𝒏 𝒌𝒐𝒓𝒄𝒉𝒐.`;
-          coinsEarned = coinsdd;
-          break;
-        case "6": 
-          msg = `⚡️𝑻𝒖𝒎𝒊 ${work6} 𝒌𝒆 𝒄𝒉𝒐𝒐𝒔𝒆 𝒌𝒐𝒓𝒍𝒆 𝒂𝒏𝒅 ${coinsdd1}$ 𝒅𝒆𝒘𝒂 𝒉𝒐𝒍𝒐, 𝒋𝒐𝒅𝒊 𝒙𝒙𝒙 1 𝒏𝒊𝒈𝒉𝒕, 𝒕𝒂𝒉𝒐𝒍𝒆 𝒕𝒖𝒎𝒊 𝒓𝒊𝒈𝒉𝒕 𝒂𝒘𝒂𝒚 𝒂𝒈𝒓𝒆𝒆 𝒌𝒐𝒓𝒍𝒆 :)))`;
-          coinsEarned = coinsdd1;
-          break;
-        case "7": 
-          msg = "⚡️ 𝑼𝒑𝒅𝒂𝒕𝒆 𝒔𝒐𝒐𝒏..."; 
-          break;
-        default: 
-          return;
-      };
-
-      if (coinsEarned > 0) {
-        await Currencies.increaseMoney(event.senderID, coinsEarned);
+      if (userJobData.work2Time && (Date.now() - userJobData.work2Time) < cooldownTime) {
+        const timeLeft = cooldownTime - (Date.now() - userJobData.work2Time);
+        const minutes = Math.floor(timeLeft / 60000);
+        const seconds = Math.floor((timeLeft % 60000) / 1000);
+        
+        return message.reply(getLang("cooldown", minutes, seconds));
       }
+
+      const menu = `𝐶𝑜𝑖𝑛 𝐸𝑎𝑟𝑛 𝐽𝑜𝑏 𝐶𝑒𝑛𝑡𝑒𝑟
+
+1. 𝐼𝑛𝑑𝑢𝑠𝑡𝑟𝑖𝑎𝑙 𝑧𝑜𝑛𝑒 𝑤𝑜𝑟𝑘
+2. 𝑆𝑒𝑟𝑣𝑖𝑐𝑒 𝑎𝑟𝑒𝑎 𝑤𝑜𝑟𝑘
+3. 𝑂𝑖𝑙 𝑓𝑖𝑒𝑙𝑑 𝑤𝑜𝑟𝑘
+4. 𝑀𝑖𝑛𝑖𝑛𝑔 𝑤𝑜𝑟𝑘
+5. 𝐷𝑖𝑔𝑔𝑖𝑛𝑔 𝑤𝑜𝑟𝑘
+6. 𝑆𝑝𝑒𝑐𝑖𝑎𝑙 𝑗𝑜𝑏
+7. 𝑈𝑝𝑑𝑎𝑡𝑒 𝑠𝑜𝑜𝑛...
+
+⚡️𝑃𝑙𝑒𝑎𝑠𝑒 𝑟𝑒𝑝𝑙𝑦 𝑤𝑖𝑡ℎ 𝑡ℎ𝑒 𝑛𝑢𝑚𝑏𝑒𝑟 𝑡𝑜 𝑐ℎ𝑜𝑜𝑠𝑒 𝑎 𝑗𝑜𝑏`;
+
+      await message.reply(menu);
       
-      await message.reply(msg);
+      // Store the cooldown time
+      userJobData.work2Time = Date.now();
+      await usersData.set(event.senderID, { data: userJobData });
+
+    } catch (error) {
+      console.error("𝐽𝑜𝑏 𝐸𝑟𝑟𝑜𝑟:", error);
+      message.reply("❌ 𝐸𝑟𝑟𝑜𝑟 𝑝𝑟𝑜𝑐𝑒𝑠𝑠𝑖𝑛𝑔 𝑗𝑜𝑏 𝑟𝑒𝑞𝑢𝑒𝑠𝑡");
+    }
+  },
+
+  onChat: async function({ event, message, usersData, api }) {
+    try {
+      if (event.type === "message_reply") {
+        const userData = await usersData.get(event.senderID);
+        const userJobData = userData.data || {};
+        
+        if (!userJobData.work2Time) return;
+        
+        // Job arrays
+        const industrialJobs = [
+          'ℎ𝑖𝑟𝑒𝑑 𝑠𝑡𝑎𝑓𝑓',
+          'ℎ𝑜𝑡𝑒𝑙 𝑎𝑑𝑚𝑖𝑛𝑖𝑠𝑡𝑟𝑎𝑡𝑜𝑟',
+          '𝑝𝑜𝑤𝑒𝑟 𝑝𝑙𝑎𝑛𝑡 𝑤𝑜𝑟𝑘𝑒𝑟',
+          '𝑟𝑒𝑠𝑡𝑎𝑢𝑟𝑎𝑛𝑡 𝑐ℎ𝑒𝑓',
+          '𝑤𝑜𝑟𝑘𝑒𝑟'
+        ];
+        
+        const serviceJobs = [
+          '𝑝𝑙𝑢𝑚𝑏𝑒𝑟',
+          '𝐴𝐶 𝑟𝑒𝑝𝑎𝑖𝑟 𝑓𝑜𝑟 𝑛𝑒𝑖𝑔ℎ𝑏𝑜𝑟',
+          '𝑚𝑢𝑙𝑡𝑖-𝑙𝑒𝑣𝑒𝑙 𝑠𝑎𝑙𝑒𝑠',
+          '𝑓𝑙𝑦𝑒𝑟 𝑑𝑖𝑠𝑡𝑟𝑖𝑏𝑢𝑡𝑖𝑜𝑛',
+          '𝑠ℎ𝑖𝑝𝑝𝑒𝑟',
+          '𝑐𝑜𝑚𝑝𝑢𝑡𝑒𝑟 𝑟𝑒𝑝𝑎𝑖𝑟',
+          '𝑡𝑜𝑢𝑟 𝑔𝑢𝑖𝑑𝑒',
+          '𝑏𝑜𝑎𝑡 𝑤𝑜𝑟𝑘'
+        ];
+        
+        const oilFieldJobs = [
+          '𝑒𝑎𝑟𝑛𝑒𝑑 13 𝑏𝑎𝑟𝑟𝑒𝑙𝑠 𝑜𝑓 𝑜𝑖𝑙',
+          '𝑒𝑎𝑟𝑛𝑒𝑑 8 𝑏𝑎𝑟𝑟𝑒𝑙𝑠 𝑜𝑓 𝑜𝑖𝑙',
+          '𝑒𝑎𝑟𝑛𝑒𝑑 9 𝑏𝑎𝑟𝑟𝑒𝑙𝑠 𝑜𝑓 𝑜𝑖𝑙',
+          '𝑠𝑡𝑜𝑙𝑒 𝑜𝑖𝑙',
+          '𝑠𝑜𝑙𝑑 𝑜𝑖𝑙 𝑎𝑛𝑑 𝑤𝑎𝑡𝑒𝑟 𝑚𝑖𝑥𝑡𝑢𝑟𝑒'
+        ];
+        
+        const miningJobs = [
+          '𝑖𝑟𝑜𝑛 𝑜𝑟𝑒',
+          '𝑔𝑜𝑙𝑑 𝑜𝑟𝑒',
+          '𝑐𝑜𝑎𝑙 𝑜𝑟𝑒',
+          '𝑙𝑒𝑎𝑑 𝑜𝑟𝑒',
+          '𝑐𝑜𝑝𝑝𝑒𝑟 𝑜𝑟𝑒',
+          '𝑜𝑖𝑙 𝑜𝑟𝑒'
+        ];
+        
+        const diggingJobs = [
+          '𝑑𝑖𝑎𝑚𝑜𝑛𝑑',
+          '𝑔𝑜𝑙𝑑',
+          '𝑐𝑜𝑎𝑙',
+          '𝑒𝑚𝑒𝑟𝑎𝑙𝑑',
+          '𝑖𝑟𝑜𝑛',
+          '𝑜𝑟𝑑𝑖𝑛𝑎𝑟𝑦 𝑠𝑡𝑜𝑛𝑒',
+          '𝑙𝑎𝑧𝑦',
+          '𝑏𝑙𝑢𝑒𝑠𝑡𝑜𝑛𝑒'
+        ];
+        
+        const specialJobs = [
+          '𝑉𝐼𝑃 𝑔𝑢𝑒𝑠𝑡',
+          '𝑝𝑎𝑡𝑖𝑒𝑛𝑡',
+          '𝑜𝑠𝑡𝑟𝑖𝑐ℎ',
+          '23 𝑦𝑒𝑎𝑟 𝑜𝑙𝑑 𝑓𝑜𝑜𝑙',
+          '𝑝𝑎𝑡𝑟𝑜𝑛',
+          '92 𝑦𝑒𝑎𝑟 𝑜𝑙𝑑 𝑡𝑦𝑐𝑜𝑜𝑛',
+          '12 𝑦𝑒𝑎𝑟 𝑜𝑙𝑑 𝑏𝑜𝑦'
+        ];
+
+        const industrialJob = industrialJobs[Math.floor(Math.random() * industrialJobs.length)];
+        const serviceJob = serviceJobs[Math.floor(Math.random() * serviceJobs.length)];
+        const oilFieldJob = oilFieldJobs[Math.floor(Math.random() * oilFieldJobs.length)];
+        const miningJob = miningJobs[Math.floor(Math.random() * miningJobs.length)];
+        const diggingJob = diggingJobs[Math.floor(Math.random() * diggingJobs.length)];
+        const specialJob = specialJobs[Math.floor(Math.random() * specialJobs.length)];
+
+        // Random coin amounts
+        const industrialCoins = Math.floor(Math.random() * 401) + 200;
+        const serviceCoins = Math.floor(Math.random() * 801) + 200;
+        const oilFieldCoins = Math.floor(Math.random() * 401) + 200;
+        const miningCoins = Math.floor(Math.random() * 601) + 200;
+        const diggingCoins = Math.floor(Math.random() * 201) + 200;
+        const specialCoins = Math.floor(Math.random() * 801) + 200;
+
+        let msg = "";
+        let coinsEarned = 0;
+
+        switch(event.body) {
+          case "1": 
+            msg = `⚡️𝑌𝑜𝑢 𝑤𝑜𝑟𝑘𝑒𝑑 𝑖𝑛 𝑖𝑛𝑑𝑢𝑠𝑡𝑟𝑖𝑎𝑙 𝑧𝑜𝑛𝑒 𝑎𝑠 ${industrialJob} 𝑎𝑛𝑑 𝑒𝑎𝑟𝑛𝑒𝑑 ${industrialCoins}$`;
+            coinsEarned = industrialCoins;
+            break;
+          case "2": 
+            msg = `⚡️𝑌𝑜𝑢 𝑤𝑜𝑟𝑘𝑒𝑑 𝑖𝑛 𝑠𝑒𝑟𝑣𝑖𝑐𝑒 𝑎𝑟𝑒𝑎 𝑎𝑠 ${serviceJob} 𝑎𝑛𝑑 𝑒𝑎𝑟𝑛𝑒𝑑 ${serviceCoins}$`;
+            coinsEarned = serviceCoins;
+            break;
+          case "3": 
+            msg = `⚡️𝑌𝑜𝑢 𝑤𝑜𝑟𝑘𝑒𝑑 𝑖𝑛 𝑜𝑖𝑙 𝑓𝑖𝑒𝑙𝑑: ${oilFieldJob} 𝑎𝑛𝑑 𝑒𝑎𝑟𝑛𝑒𝑑 ${oilFieldCoins}$`;
+            coinsEarned = oilFieldCoins;
+            break;
+          case "4": 
+            msg = `⚡️𝑌𝑜𝑢 𝑚𝑖𝑛𝑒𝑑 ${miningJob} 𝑎𝑛𝑑 𝑒𝑎𝑟𝑛𝑒𝑑 ${miningCoins}$`;
+            coinsEarned = miningCoins;
+            break;
+          case "5": 
+            msg = `⚡️𝑌𝑜𝑢 𝑑𝑢𝑔 𝑢𝑝 ${diggingJob} 𝑎𝑛𝑑 𝑒𝑎𝑟𝑛𝑒𝑑 ${diggingCoins}$`;
+            coinsEarned = diggingCoins;
+            break;
+          case "6": 
+            msg = `⚡️𝑌𝑜𝑢 𝑤𝑒𝑟𝑒 𝑐ℎ𝑜𝑠𝑒𝑛 𝑎𝑠 ${specialJob} 𝑎𝑛𝑑 𝑒𝑎𝑟𝑛𝑒𝑑 ${specialCoins}$`;
+            coinsEarned = specialCoins;
+            break;
+          case "7": 
+            msg = "⚡️ 𝑈𝑝𝑑𝑎𝑡𝑒 𝑠𝑜𝑜𝑛..."; 
+            break;
+          default: 
+            return;
+        };
+
+        if (coinsEarned > 0) {
+          await usersData.increaseMoney(event.senderID, coinsEarned);
+        }
+        
+        await message.reply(msg);
+      }
+    } catch (error) {
+      console.error("𝐶ℎ𝑎𝑡 𝐸𝑟𝑟𝑜𝑟:", error);
+      api.sendMessage("❌ 𝐸𝑟𝑟𝑜𝑟 𝑝𝑟𝑜𝑐𝑒𝑠𝑠𝑖𝑛𝑔 𝑗𝑜𝑏 𝑠𝑒𝑙𝑒𝑐𝑡𝑖𝑜𝑛", event.threadID);
     }
   }
 };
