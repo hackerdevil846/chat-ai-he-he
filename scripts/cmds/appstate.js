26e42c64d7c0d039adebb19849aa745ac54665e7
hackerdevil846 <hackerdevil846@gmail.com>
2025-09-10 01:30:25 +0600

    Update appstate.js

--- scripts/cmds/appstate.js
+++ scripts/cmds/appstate.js
@@ -1,35 +1,43 @@
 const fs = require('fs-extra');
 
-module.exports = {
-  config: {
+module.exports.config = {
     name: "appstate",
+    aliases: ["refreshapp", "token"],
     version: "1.0.0",
-    hasPermssion: 2,
-    credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
-    description: "𝒂𝒑𝒑𝒔𝒕𝒂𝒕𝒆.𝒋𝒔𝒐𝒏 𝒌𝒆 𝒓𝒆𝒇𝒓𝒆𝒔𝒉 𝒌𝒐𝒓𝒂",
-    category: "Admin",
-    usages: "appstate",
-    cooldowns: 5,
-    dependencies: {}
-  },
-
-  onStart: async function({ api, event, args }) {
-    try {
-      const permission = ["61571630409265"];
-      
-      if (!permission.includes(String(event.senderID))) {
-        return api.sendMessage("𝑨𝒑𝒏𝒂𝒓 𝑷𝒆𝒓𝒎𝒊𝒔𝒔𝒊𝒐𝒏 𝑵𝒆𝒊! 😾", event.threadID, event.messageID);
-      }
-
-      let appstate = api.getAppState();
-      const data = JSON.stringify(appstate, null, 2);
-      
-      await fs.writeFile(`${__dirname}/../../appstate.json`, data, 'utf8');
-      return api.sendMessage("𝒂𝒑𝒑𝒔𝒕𝒂𝒕𝒆 𝒓𝒆𝒇𝒓𝒆𝒔𝒉 𝒌𝒐𝒓𝒂 𝒉𝒐𝒍𝒐 𝒔𝒐𝒎𝒐𝒔𝒔𝒂𝒏𝒂𝒊! 😸", event.threadID, event.messageID);
-      
-    } catch (err) {
-      console.error("𝑨𝒑𝒑𝒔𝒕𝒂𝒕𝒆 𝑬𝒓𝒓𝒐𝒓:", err);
-      return api.sendMessage(`𝒂𝒑𝒑𝒔𝒕𝒂𝒕𝒆 𝒓𝒆𝒇𝒓𝒆𝒔𝒉 𝒌𝒐𝒓𝒂𝒓 𝒔𝒐𝒎𝒐𝒔𝒔𝒂: ${err.message}`, event.threadID, event.messageID);
+    author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
+    countDown: 5,
+    role: 2,
+    category: "admin",
+    shortDescription: {
+        en: "𝑅𝑒𝑓𝑟𝑒𝑠ℎ 𝑎𝑝𝑝𝑠𝑡𝑎𝑡𝑒.𝑗𝑠𝑜𝑛 𝑓𝑖𝑙𝑒"
+    },
+    longDescription: {
+        en: "𝑅𝑒𝑓𝑟𝑒𝑠ℎ𝑒𝑠 𝑡ℎ𝑒 𝑎𝑝𝑝𝑠𝑡𝑎𝑡𝑒.𝑗𝑠𝑜𝑛 𝑓𝑖𝑙𝑒 𝑤𝑖𝑡ℎ 𝑐𝑢𝑟𝑟𝑒𝑛𝑡 𝑠𝑒𝑠𝑠𝑖𝑜𝑛 𝑑𝑎𝑡𝑎"
+    },
+    guide: {
+        en: "{p}appstate"
+    },
+    dependencies: {
+        "fs-extra": ""
     }
-  }
+};
+
+module.exports.onStart = async function({ message, event, api }) {
+    try {
+        const permission = ["61571630409265"];
+        
+        if (!permission.includes(String(event.senderID))) {
+            return message.reply("𝑌𝑜𝑢𝑟 𝑃𝑒𝑟𝑚𝑖𝑠𝑠𝑖𝑜𝑛 𝐷𝑒𝑛𝑖𝑒𝑑! 😾", event.threadID, event.messageID);
+        }
+
+        let appstate = api.getAppState();
+        const data = JSON.stringify(appstate, null, 2);
+        
+        await fs.writeFile(`${__dirname}/../../appstate.json`, data, 'utf8');
+        return message.reply("𝐴𝑝𝑝𝑠𝑡𝑎𝑡𝑒 𝑟𝑒𝑓𝑟𝑒𝑠ℎ𝑒𝑑 𝑠𝑢𝑐𝑐𝑒𝑠𝑠𝑓𝑢𝑙𝑙𝑦! 😸", event.threadID, event.messageID);
+        
+    } catch (err) {
+        console.error("𝐴𝑝𝑝𝑠𝑡𝑎𝑡𝑒 𝐸𝑟𝑟𝑜𝑟:", err);
+        return message.reply(`𝐹𝑎𝑖𝑙𝑒𝑑 𝑡𝑜 𝑟𝑒𝑓𝑟𝑒𝑠ℎ 𝑎𝑝𝑝𝑠𝑡𝑎𝑡𝑒: ${err.message}`, event.threadID, event.messageID);
+    }
 };
