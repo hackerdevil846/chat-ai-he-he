c26b1e13893cea1aee5ee4b5b7332a0f5047b18b
hackerdevil846 <hackerdevil846@gmail.com>
2025-09-14 19:56:09 +0600

    Update lizard.js

--- scripts/cmds/lizard.js
+++ scripts/cmds/lizard.js
@@ -1,18 +1,24 @@
 module.exports = {
     config: {
         name: "lizard",
+        aliases: ["lizardpic", "reptile"],
         version: "1.0.0",
         author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
+        countDown: 5,
         role: 0,
         category: "image",
         shortDescription: {
             en: "𝑅𝑎𝑛𝑑𝑜𝑚 𝑙𝑖𝑧𝑎𝑟𝑑 𝑖𝑚𝑎𝑔𝑒𝑠"
         },
         longDescription: {
-            en: "𝐷𝑖𝑠𝑝𝑙𝑎𝑦𝑠 𝑟𝑎𝑛𝑑𝑜𝑚 𝑙𝑖𝑧𝑎𝑟𝑑 𝑝𝑖𝑐𝑡𝑢𝑟𝑒𝑠"
+            en: "𝐷𝑖𝑠𝑝𝑙𝑎𝑦𝑠 𝑟𝑎𝑛𝑑𝑜𝑚 𝑙𝑖𝑧𝑎𝑟𝑑 𝑝𝑖𝑐𝑡𝑢𝑟𝑒𝑠 𝑓𝑟𝑜𝑚 𝑎𝑛 𝐴𝑃𝐼"
         },
         guide: {
-            en: "{𝑝}𝑙𝑖𝑧𝑎𝑟𝑑"
+            en: "{p}lizard"
+        },
+        dependencies: {
+            "axios": "",
+            "fs-extra": ""
         }
     },
 
@@ -25,36 +31,15 @@ module.exports = {
             // Get random lizard image from API
             const response = await axios.get('https://nekos.life/api/v2/img/lizard');
             const imageUrl = response.data.url;
-            const fileExtension = imageUrl.substring(imageUrl.lastIndexOf(".") + 1);
             
-            // Create cache directory if it doesn't exist
-            const cacheDir = path.resolve(__dirname, 'cache');
-            if (!fs.existsSync(cacheDir)) {
-                fs.mkdirSync(cacheDir, { recursive: true });
-            }
-
-            const filePath = path.resolve(cacheDir, `lizard.${fileExtension}`);
-            
-            // Download the image
-            const imageResponse = await axios.get(imageUrl, { 
-                responseType: 'arraybuffer' 
-            });
-            
-            // Save image to cache
-            fs.writeFileSync(filePath, Buffer.from(imageResponse.data, 'binary'));
+            // Download the image using global utility
+            const imageStream = await global.utils.getStreamFromURL(imageUrl);
             
             // Send the image
             await message.reply({
                 body: "🦎 𝐻𝑒𝑟𝑒'𝑠 𝑦𝑜𝑢𝑟 𝑟𝑎𝑛𝑑𝑜𝑚 𝑙𝑖𝑧𝑎𝑟𝑑 𝑖𝑚𝑎𝑔𝑒!",
-                attachment: fs.createReadStream(filePath)
+                attachment: imageStream
             });
-            
-            // Clean up after sending
-            setTimeout(() => {
-                if (fs.existsSync(filePath)) {
-                    fs.unlinkSync(filePath);
-                }
-            }, 5000);
             
         } catch (error) {
             console.error("𝐿𝑖𝑧𝑎𝑟𝑑 𝑒𝑟𝑟𝑜𝑟:", error);
