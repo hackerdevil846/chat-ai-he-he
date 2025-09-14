c343fccf8ef1675ce20a9909f70b65821d7e9651
hackerdevil846 <hackerdevil846@gmail.com>
2025-09-14 16:23:14 +0600

    Update jack.js

--- scripts/cmds/jack.js
+++ scripts/cmds/jack.js
@@ -1,126 +1,132 @@
-module.exports = {
-  config: {
+const axios = require("axios");
+const fs = require("fs-extra");
+const path = require("path");
+
+module.exports.config = {
     name: "jack",
-    aliases: ["jack"],
+    aliases: ["jackimg"],
     version: "1.0.0",
     author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
     countDown: 5,
     role: 0,
     shortDescription: {
-      en: "𝑅𝑎𝑛𝑑𝑜𝑚 𝐽𝑎𝑐𝑘 𝑖𝑚𝑎𝑔𝑒𝑠"
+        en: "𝑅𝑎𝑛𝑑𝑜𝑚 𝐽𝑎𝑐𝑘 𝑖𝑚𝑎𝑔𝑒𝑠"
     },
     longDescription: {
-      en: "𝑆𝑒𝑛𝑑𝑠 𝑟𝑎𝑛𝑑𝑜𝑚 𝐽𝑎𝑐𝑘 𝑖𝑚𝑎𝑔𝑒𝑠 𝑤𝑖𝑡ℎ 𝑎 𝑐𝑜𝑠𝑡 𝑜𝑓 10 𝑚𝑜𝑛𝑒𝑦"
+        en: "𝑆𝑒𝑛𝑑𝑠 𝑟𝑎𝑛𝑑𝑜𝑚 𝐽𝑎𝑐𝑘 𝑖𝑚𝑎𝑔𝑒𝑠 𝑤𝑖𝑡ℎ 𝑎 𝑐𝑜𝑠𝑡 𝑜𝑓 10 𝑚𝑜𝑛𝑒𝑦"
     },
     category: "𝑓𝑢𝑛",
     guide: {
-      en: "{𝑝}𝑗𝑎𝑐𝑘"
+        en: "{p}jack"
+    },
+    dependencies: {
+        "axios": "",
+        "fs-extra": ""
     }
-  },
+};
 
-  onStart: async function ({ api, event, args, Currencies }) {
+module.exports.onStart = async function ({ message, event, Currencies }) {
     try {
-      const fs = global.nodemodule["fs-extra"];
-      const axios = global.nodemodule["axios"];
-      const path = global.nodemodule["path"];
-      
-      const link = [
+        const link = [
+            "https://i.pinimg.com/236x/0a/fc/14/0afc14a83ac615c14df96fbb120e4023.jpg",
+            "https://i.pinimg.com/236x/36/26/dc/3626dce9c4e7124783e03c1e49d07a68.jpg",
+            "https://i.pinimg.com/236x/43/44/ab/4344ab78bb4d71bc9b8c1d067e1016a7.jpg",
+            "https://i.pinimg.com/236x/7e/15/75/7e1575629035d940797ad97cedecb915.jpg",
+            "https://i.pinimg.com/236x/ad/d6/8f/add68f2eeb612988d5a692d041d0d5c9.jpg",
+            "https://i.pinimg.com/236x/e0/e6/9e/e0e69ed84dc5bd21bdef7d3b40d5e12c.jpg",
+            "https://i.pinimg.com/236x/61/83/94/618394956793dff58dd1a6eee1a90643.jpg",
+            "https://i.pinimg.com/236x/08/60/a1/0860a188b69914201e94a3c120e4e292.jpg",
+            "https://i.pinimg.com/236x/ac/23/cb/ac23cb124d5d0c8db30b9c9f1f3e2a96.jpg",
+            "https://i.pinimg.com/236x/0c/26/d0/0c26d08a9187cdc3a2198ea08172c475.jpg",
+            "https://i.pinimg.com/236x/54/99/0f/54990f8a68a198eaf77e0d3ecbd1d1df.jpg",
+            "https://i.pinimg.com/236x/47/d1/ef/47d1efa8fded2d244103271e728e7167.jpg",
+            "https://i.pinimg.com/236x/a1/36/f3/a136f3fc090f4eaf2bdda3cacd93320c.jpg",
+            "https://i.pinimg.com/236x/a1/02/52/a10252a12a944ef50f9a1ee542a42644.jpg",
+            "https://i.pinimg.com/236x/38/0a/e4/380ae422b026ccdf97db5b5c6eabc272.jpg",
+            "https://i.pinimg.com/236x/f0/39/7b/f0397b16c939f81042e2a8a109a36bd5.jpg",
+            "https://i.pinimg.com/236x/74/d8/b6/74d8b64b3717bcc7b4b7b05d44d05152.jpg",
+            "https://i.pinimg.com/236x/c6/57/ab/c657ab169227b6b32401acf703bfec98.jpg",
+            "https://i.pinimg.com/236x/de/15/3e/de153e52550fd9258098dca4281b5e56.jpg",
+            "https://i.pinimg.com/236x/51/ca/fe/51cafeff6e5355f69cda2268b7e80bf8.jpg",
+            "https://i.pinimg.com/236x/8b/0b/fc/8b0bfc768c260f8983ccd9803f480007.jpg",
+            "https://i.pinimg.com/236x/b5/27/95/b527959ea7ad5777a2c8ad869e04f56d.jpg",
+            "https://i.pinimg.com/236x/d2/82/a5/d282a57ee616d6ef53fceb26eb460d32.jpg",
+            "https://i.pinimg.com/236x/b5/27/95/b527959ea7ad5777a2c8ad869e04f56d.jpg",
+            "https://i.pinimg.com/236x/e4/05/91/e40591ed588cf172b527d10ea2b0313b.jpg",
+            "https://i.pinimg.com/236x/3c/78/c1/3c78c1930973f665f016d9af73fc7da3.jpg",
+            "https://i.pinimg.com/236x/27/d8/09/27d8090c289552da29a00ee1334e6ed0.jpg",
+            "https://i.pinimg.com/236x/50/36/a9/5036a9074ff67834519d30d6a1cbcce6.jpg",
+            "https://i.pinimg.com/236x/04/d8/f8/04d8f875969f1b5eebeaf46c4197dbc0.jpg",
+            "https://i.pinimg.com/236x/51/4e/49/514e4941db299e5cdd98c01427fea9a3.jpg",
+            "https://i.pinimg.com/236x/a7/62/7d/a7627d34d11ca1e94451b99ae252ff81.jpg",
+            "https://i.pinimg.com/236x/bc/cc/b4/bcccb4d6e1df8574403baaf24b88b98e.jpg",
+            "https://i.pinimg.com/236x/7f/00/cd/7f00cdc8a7102385e3bf66a6bef4eb8c.jpg",
+            "https://i.pinimg.com/236x/21/64/10/2164106a1383ba0b7ff03821cbd6ed37.jpg",
+            "https://i.pinimg.com/236x/a3/87/43/a38743953171b2c17806da1a109c9fea.jpg",
+            "https://i.pinimg.com/236x/69/f5/9c/69f59cc6521074cbff9cec6547bb5139.jpg",
+            "https://i.pinimg.com/236x/8f/d9/08/8fd9083dc75adf8c91a63cad6abdd807.jpg",
+            "https://i.pinimg.com/236x/18/42/80/184280bcb7a6ab240c14a9b8c4ffa732.jpg",
+            "https://i.pinimg.com/236x/1d/bf/8f/1dbf8f3e1483599449238f1366ac05bf.jpg",
+            "https://i.pinimg.com/236x/cf/2d/42/cf2d4217c8242355e4d01a8004c2d602.jpg",
+            "https://i.pinimg.com/236x/73/47/0b/73470b35e6738b81db584d74aebf92dd.jpg",
+            "https://i.pinimg.com/236x/48/31/82/4831827e3b04c33d559792c5ed217c3d.jpg",
+            "https://i.pinimg.com/236x/dd/fb/f5/ddfbf5b1837414b27249257a2edaad74.jpg",
+            "https://i.pinimg.com/236x/ac/7a/9a/ac7a9a2a579f44094b2754c76d3f0093.jpg",
+            "https://i.pinimg.com/236x/9a/99/d7/9a99d732e76f71f75585ff2b5dfbb564.jpg",
+            "https://i.pinimg.com/236x/ca/06/ba/ca06ba280ccd070595d7cdc2a09baab9.jpg",
+            "https://i.pinimg.com/236x/55/d2/1e/55d21e723aea77dd9c27542b942099e0.jpg",
+            "https://i.pinimg.com/236x/5d/83/f8/5d83f8e31e90c1957599cdce665c7a01.jpg",
+            "https://i.pinimg.com/236x/d2/84/a6/d284a68cdcefd5089e0b30e08179958e.jpg",
+            "https://i.pinimg.com/236x/d0/f2/61/d0f26164c8d6c5d1122e3e77a0ce1829.jpg",
+            "https://i.pinimg.com/236x/2c/dd/3a/2cdd3a2352c3f51845529dc4e84bd5ce.jpg",
+            "https://i.pinimg.com/236x/74/1e/67/741e67122ccc43a52d6b152c105add75.jpg",
+            "https://i.pinimg.com/236x/8a/9d/89/8a9d89cfd689637c9b3398ed408a2761.jpg",
+            "https://i.pinimg.com/236x/e2/04/02/e204021fe9fbf4f0c4f9b8f2fed3ff24.jpg",
+            "https://i.pinimg.com/236x/d3/09/4f/d3094f33f6214f40e0ece64138c7faec.jpg",
+            "https://i.pinimg.com/236x/08/60/a1/0860a188b69914201e94a3c120e4e292.jpg",
+            "https://i.pinimg.com/236x/e9/16/2f/e9162f3ab8314700b2bc89dd3a656616.jpg",
+            "https://i.pinimg.com/236x/1c/07/4d/1c074d0319af86aef1812ab88ae4ede1.jpg",
+            "https://i.pinimg.com/236x/8a/2f/75/8a2f752d6061959471971bc843cecf6f.jpg",
+            "https://i.pinimg.com/236x/a3/b2/46/a3b246d8e3c48ba50283cda5a357c2aa.jpg"
+        ];
 
-"https://i.pinimg.com/236x/0a/fc/14/0afc14a83ac615c14df96fbb120e4023.jpg",
-"https://i.pinimg.com/236x/36/26/dc/3626dce9c4e7124783e03c1e49d07a68.jpg",
-"https://i.pinimg.com/236x/43/44/ab/4344ab78bb4d71bc9b8c1d067e1016a7.jpg",
-"https://i.pinimg.com/236x/7e/15/75/7e1575629035d940797ad97cedecb915.jpg",
-"https://i.pinimg.com/236x/ad/d6/8f/add68f2eeb612988d5a692d041d0d5c9.jpg",
-"https://i.pinimg.com/236x/e0/e6/9e/e0e69ed84dc5bd21bdef7d3b40d5e12c.jpg",
-"https://i.pinimg.com/236x/61/83/94/618394956793dff58dd1a6eee1a90643.jpg",
-"https://i.pinimg.com/236x/08/60/a1/0860a188b69914201e94a3c120e4e292.jpg",
-"https://i.pinimg.com/236x/ac/23/cb/ac23cb124d5d0c8db30b9c9f1f3e2a96.jpg",
-"https://i.pinimg.com/236x/0c/26/d0/0c26d08a9187cdc3a2198ea08172c475.jpg",
-"https://i.pinimg.com/236x/54/99/0f/54990f8a68a198eaf77e0d3ecbd1d1df.jpg",
-"https://i.pinimg.com/236x/47/d1/ef/47d1efa8fded2d244103271e728e7167.jpg",
-"https://i.pinimg.com/236x/a1/36/f3/a136f3fc090f4eaf2bdda3cacd93320c.jpg",
-"https://i.pinimg.com/236x/a1/02/52/a10252a12a944ef50f9a1ee542a42644.jpg",
-"https://i.pinimg.com/236x/38/0a/e4/380ae422b026ccdf97db5b5c6eabc272.jpg",
-"https://i.pinimg.com/236x/f0/39/7b/f0397b16c939f81042e2a8a109a36bd5.jpg",
-"https://i.pinimg.com/236x/74/d8/b6/74d8b64b3717bcc7b4b7b05d44d05152.jpg",
-"https://i.pinimg.com/236x/c6/57/ab/c657ab169227b6b32401acf703bfec98.jpg",
-"https://i.pinimg.com/236x/de/15/3e/de153e52550fd9258098dca4281b5e56.jpg",
-"https://i.pinimg.com/236x/51/ca/fe/51cafeff6e5355f69cda2268b7e80bf8.jpg",
-"https://i.pinimg.com/236x/8b/0b/fc/8b0bfc768c260f8983ccd9803f480007.jpg",
-"https://i.pinimg.com/236x/b5/27/95/b527959ea7ad5777a2c8ad869e04f56d.jpg",
-"https://i.pinimg.com/236x/d2/82/a5/d282a57ee616d6ef53fceb26eb460d32.jpg",
-"https://i.pinimg.com/236x/b5/27/95/b527959ea7ad5777a2c8ad869e04f56d.jpg",
-"https://i.pinimg.com/236x/e4/05/91/e40591ed588cf172b527d10ea2b0313b.jpg",
-"https://i.pinimg.com/236x/3c/78/c1/3c78c1930973f665f016d9af73fc7da3.jpg",
-"https://i.pinimg.com/236x/27/d8/09/27d8090c289552da29a00ee1334e6ed0.jpg",
-"https://i.pinimg.com/236x/50/36/a9/5036a9074ff67834519d30d6a1cbcce6.jpg",
-"https://i.pinimg.com/236x/04/d8/f8/04d8f875969f1b5eebeaf46c4197dbc0.jpg",
-"https://i.pinimg.com/236x/51/4e/49/514e4941db299e5cdd98c01427fea9a3.jpg",
-"https://i.pinimg.com/236x/a7/62/7d/a7627d34d11ca1e94451b99ae252ff81.jpg",
-"https://i.pinimg.com/236x/bc/cc/b4/bcccb4d6e1df8574403baaf24b88b98e.jpg",
-"https://i.pinimg.com/236x/7f/00/cd/7f00cdc8a7102385e3bf66a6bef4eb8c.jpg",
-"https://i.pinimg.com/236x/21/64/10/2164106a1383ba0b7ff03821cbd6ed37.jpg",
-"https://i.pinimg.com/236x/a3/87/43/a38743953171b2c17806da1a109c9fea.jpg",
-"https://i.pinimg.com/236x/69/f5/9c/69f59cc6521074cbff9cec6547bb5139.jpg",
-"https://i.pinimg.com/236x/8f/d9/08/8fd9083dc75adf8c91a63cad6abdd807.jpg",
-"https://i.pinimg.com/236x/18/42/80/184280bcb7a6ab240c14a9b8c4ffa732.jpg",
-"https://i.pinimg.com/236x/1d/bf/8f/1dbf8f3e1483599449238f1366ac05bf.jpg",
-"https://i.pinimg.com/236x/cf/2d/42/cf2d4217c8242355e4d01a8004c2d602.jpg",
-"https://i.pinimg.com/236x/73/47/0b/73470b35e6738b81db584d74aebf92dd.jpg",
-"https://i.pinimg.com/236x/48/31/82/4831827e3b04c33d559792c5ed217c3d.jpg",
-"https://i.pinimg.com/236x/dd/fb/f5/ddfbf5b1837414b27249257a2edaad74.jpg",
-"https://i.pinimg.com/236x/ac/7a/9a/ac7a9a2a579f44094b2754c76d3f0093.jpg",
-"https://i.pinimg.com/236x/9a/99/d7/9a99d732e76f71f75585ff2b5dfbb564.jpg",
-"https://i.pinimg.com/236x/ca/06/ba/ca06ba280ccd070595d7cdc2a09baab9.jpg",
-"https://i.pinimg.com/236x/55/d2/1e/55d21e723aea77dd9c27542b942099e0.jpg",
-"https://i.pinimg.com/236x/5d/83/f8/5d83f8e31e90c1957599cdce665c7a01.jpg",
-"https://i.pinimg.com/236x/d2/84/a6/d284a68cdcefd5089e0b30e08179958e.jpg",
-"https://i.pinimg.com/236x/d0/f2/61/d0f26164c8d6c5d1122e3e77a0ce1829.jpg",
-"https://i.pinimg.com/236x/2c/dd/3a/2cdd3a2352c3f51845529dc4e84bd5ce.jpg",
-"https://i.pinimg.com/236x/74/1e/67/741e67122ccc43a52d6b152c105add75.jpg",
-"https://i.pinimg.com/236x/8a/9d/89/8a9d89cfd689637c9b3398ed408a2761.jpg",
-"https://i.pinimg.com/236x/e2/04/02/e204021fe9fbf4f0c4f9b8f2fed3ff24.jpg",
-"https://i.pinimg.com/236x/d3/09/4f/d3094f33f6214f40e0ece64138c7faec.jpg",
-"https://i.pinimg.com/236x/08/60/a1/0860a188b69914201e94a3c120e4e292.jpg",
-"https://i.pinimg.com/236x/e9/16/2f/e9162f3ab8314700b2bc89dd3a656616.jpg",
-"https://i.pinimg.com/236x/1c/07/4d/1c074d0319af86aef1812ab88ae4ede1.jpg",
-"https://i.pinimg.com/236x/8a/2f/75/8a2f752d6061959471971bc843cecf6f.jpg",
-"https://i.pinimg.com/236x/a3/b2/46/a3b246d8e3c48ba50283cda5a357c2aa.jpg",
+        const userData = await Currencies.getData(event.senderID);
+        const money = userData.money;
 
-];
+        if (money < 1000) {
+            return message.reply("𝒀𝒐𝒖 𝒏𝒆𝒆𝒅 1000 𝒅𝒐𝒍𝒍𝒂𝒓𝒔 𝒕𝒐 𝒗𝒊𝒆𝒘 𝒊𝒎𝒂𝒈𝒆𝒔!");
+        }
 
-      const data = await Currencies.getData(event.senderID);
-      const money = data.money;
-
-      if (money < 1000) {
-        return api.sendMessage("𝒀𝒐𝒖 𝒏𝒆𝒆𝒅 1000 𝒅𝒐𝒍𝒍𝒂𝒓𝒔 𝒕𝒐 𝒗𝒊𝒆𝒘 𝒊𝒎𝒂𝒈𝒆𝒔!", event.threadID, event.messageID);
-      } else {
         await Currencies.decreaseMoney(event.senderID, 10);
         
         const randomImage = link[Math.floor(Math.random() * link.length)];
-        const imagePath = path.resolve(__dirname, 'cache', 'jack.jpg');
+        const imagePath = path.join(__dirname, 'cache', 'jack.jpg');
         
-        const callback = () => {
-          api.sendMessage({
-            body: `𝐽𝑎𝑐𝑘 𝑖𝑚𝑎𝑔𝑒\n𝑇𝑜𝑡𝑎𝑙 𝑖𝑚𝑎𝑔𝑒𝑠: ${link.length}\n-10 𝑑𝑜𝑙𝑙𝑎𝑟𝑠!`,
-            attachment: fs.createReadStream(imagePath)
-          }, event.threadID, () => fs.unlinkSync(imagePath), event.messageID);
-        };
-
-        const response = await axios.get(randomImage, { responseType: 'stream' });
-        const writer = fs.createWriteStream(imagePath);
-        
-        response.data.pipe(writer);
-        
-        writer.on('finish', callback);
-        writer.on('error', (error) => {
-          console.error(error);
-          api.sendMessage("𝐴𝑛 𝑒𝑟𝑟𝑜𝑟 𝑜𝑐𝑐𝑢𝑟𝑟𝑒𝑑 𝑤ℎ𝑖𝑙𝑒 𝑑𝑜𝑤𝑛𝑙𝑜𝑎𝑑𝑖𝑛𝑔 𝑡ℎ𝑒 𝑖𝑚𝑎𝑔𝑒!", event.threadID, event.messageID);
+        const response = await axios({
+            method: 'GET',
+            url: randomImage,
+            responseType: 'stream'
         });
-      }
+
+        const writer = fs.createWriteStream(imagePath);
+        response.data.pipe(writer);
+
+        writer.on('finish', async () => {
+            await message.reply({
+                body: `𝐽𝑎𝑐𝑘 𝑖𝑚𝑎𝑔𝑒\n𝑇𝑜𝑡𝑎𝑙 𝑖𝑚𝑎𝑔𝑒𝑠: ${link.length}\n-10 𝑑𝑜𝑙𝑙𝑎𝑟𝑠!`,
+                attachment: fs.createReadStream(imagePath)
+            });
+            
+            fs.unlinkSync(imagePath);
+        });
+
+        writer.on('error', (error) => {
+            console.error("𝐷𝑜𝑤𝑛𝑙𝑜𝑎𝑑 𝐸𝑟𝑟𝑜𝑟:", error);
+            message.reply("𝐴𝑛 𝑒𝑟𝑟𝑜𝑟 𝑜𝑐𝑐𝑢𝑟𝑟𝑒𝑑 𝑤ℎ𝑖𝑙𝑒 𝑑𝑜𝑤𝑛𝑙𝑜𝑎𝑑𝑖𝑛𝑔 𝑡ℎ𝑒 𝑖𝑚𝑎𝑔𝑒!");
+        });
+
     } catch (error) {
-      console.error("𝐸𝑟𝑟𝑜𝑟:", error);
-      api.sendMessage("❌ 𝐴𝑛 𝑒𝑟𝑟𝑜𝑟 𝑜𝑐𝑐𝑢𝑟𝑟𝑒𝑑 𝑤ℎ𝑖𝑙𝑒 𝑒𝑥𝑒𝑐𝑢𝑡𝑖𝑛𝑔 𝑡ℎ𝑒 𝑐𝑜𝑚𝑚𝑎𝑛𝑑!", event.threadID, event.messageID);
+        console.error("𝐽𝑎𝑐𝑘 𝐸𝑟𝑟𝑜𝑟:", error);
+        message.reply("❌ 𝐴𝑛 𝑒𝑟𝑟𝑜𝑟 𝑜𝑐𝑐𝑢𝑟𝑟𝑒𝑑 𝑤ℎ𝑖𝑙𝑒 𝑒𝑥𝑒𝑐𝑢𝑡𝑖𝑛𝑔 𝑡ℎ𝑒 𝑐𝑜𝑚𝑚𝑎𝑛𝑑!");
     }
-  }
 };
