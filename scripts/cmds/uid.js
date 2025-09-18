5e06e832a31c61a7f58c9ee2b56d14b9c97b9686
hackerdevil846 <hackerdevil846@gmail.com>
2025-09-19 02:16:49 +0600

    Update uid.js

--- scripts/cmds/uid.js
+++ scripts/cmds/uid.js
@@ -1,28 +1,38 @@
 module.exports = {
   config: {
     name: "uid",
+    aliases: ["userid", "id"],
     version: "1.0.0",
-    hasPermssion: 0,
-    credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
-    description: "𝙐𝙨𝙚𝙧𝙚𝙧 𝙄𝘿 𝙟𝙖𝙣𝙩𝙚 𝙥𝙖𝙧𝙗𝙚𝙣",
-    category: "𝙏𝙤𝙤𝙡𝙨",
-    cooldowns: 5
+    author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
+    role: 0,
+    category: "tools",
+    shortDescription: {
+      en: "𝐺𝑒𝑡 𝑢𝑠𝑒𝑟 𝐼𝐷 𝑖𝑛𝑓𝑜𝑟𝑚𝑎𝑡𝑖𝑜𝑛"
+    },
+    longDescription: {
+      en: "𝐷𝑖𝑠𝑝𝑙𝑎𝑦𝑠 𝑦𝑜𝑢𝑟 𝑜𝑤𝑛 𝑢𝑠𝑒𝑟 𝐼𝐷 𝑜𝑟 𝑡ℎ𝑒 𝐼𝐷𝑠 𝑜𝑓 𝑚𝑒𝑛𝑡𝑖𝑜𝑛𝑒𝑑 𝑢𝑠𝑒𝑟𝑠"
+    },
+    guide: {
+      en: "{p}uid [@𝑚𝑒𝑛𝑡𝑖𝑜𝑛]"
+    },
+    countDown: 5
   },
 
-  onStart: async function ({ api, event }) {
-    if (Object.keys(event.mentions).length === 0) {
-      return api.sendMessage(
-        `𝘼𝙥𝙣𝙖𝙧 𝙐𝙨𝙚𝙧 𝙄𝘿: ${event.senderID}`,
-        event.threadID,
-        event.messageID
-      );
-    } else {
-      let msg = "";
-      for (const [id, name] of Object.entries(event.mentions)) {
-        const cleanName = name.replace('@', '');
-        msg += `${cleanName} - 𝙐𝙨𝙚𝙧 𝙄𝘿: ${id}\n`;
+  onStart: async function ({ api, event, message }) {
+    try {
+      if (Object.keys(event.mentions).length === 0) {
+        return message.reply(`𝒀𝒐𝒖𝒓 𝑼𝒔𝒆𝒓 𝑰𝑫: ${event.senderID}`);
+      } else {
+        let msg = "";
+        for (const [id, name] of Object.entries(event.mentions)) {
+          const cleanName = name.replace('@', '');
+          msg += `${cleanName} - 𝑼𝒔𝒆𝒓 𝑰𝑫: ${id}\n`;
+        }
+        return message.reply(msg);
       }
-      return api.sendMessage(msg, event.threadID, event.messageID);
+    } catch (error) {
+      console.error("UID Error:", error);
+      return message.reply("❌ 𝐴𝑛 𝑒𝑟𝑟𝑜𝑟 𝑜𝑐𝑐𝑢𝑟𝑟𝑒𝑑 𝑤ℎ𝑖𝑙𝑒 𝑓𝑒𝑡𝑐ℎ𝑖𝑛𝑔 𝑢𝑠𝑒𝑟 𝐼𝐷𝑠");
     }
   }
 };
