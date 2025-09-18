77eabb0c24569f70cc17798c3669dfb25a03a1ed
hackerdevil846 <hackerdevil846@gmail.com>
2025-09-18 20:10:04 +0600

    Update sub-msg.js

--- scripts/cmds/sub-msg.js
+++ scripts/cmds/sub-msg.js
@@ -1,33 +1,57 @@
-const fs = require("fs");
+const fs = require("fs-extra");
 
-module.exports.config = {
+module.exports = {
+  config: {
     name: "sub",
+    aliases: ["subscribe", "priyansh"],
     version: "1.0.1",
-    hasPermssion: 0,
-    credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
-    description: "𝙎𝙪𝙗𝙨𝙘𝙧𝙞𝙗𝙚 𝙨𝙖𝙢𝙥𝙖𝙧𝙠𝙚 𝙘𝙤𝙢𝙢𝙖𝙣𝙙",
-    category: "𝙣𝙤-𝙥𝙧𝙚𝙛𝙞𝙭",
-    usages: "sub",
-    cooldowns: 5,
-};
-
-module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
-    var { threadID, messageID } = event;
-    if (
-        event.body.indexOf("Priyansh rajput") == 0 ||
-        event.body.indexOf("Sub") == 0 ||
-        event.body.indexOf("Subscribe") == 0 ||
-        event.body.indexOf("Priyansh") == 0
-    ) {
-        var msg = {
-            body: "👋 𝙆𝙤𝙣𝙤 𝙨𝙖𝙝𝙖𝙮𝙮𝙖 𝙡𝙖𝙜𝙡𝙚 @𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝙪𝒅 𝙠𝙚 𝙘𝙤𝙣𝙩𝙖𝙘𝙩 𝙠𝙤𝙧𝙪𝙣 😇",
-            attachment: fs.createReadStream(__dirname + `/noprefix/sub.mp3`)
-        }
-        api.sendMessage(msg, threadID, messageID);
-        api.setMessageReaction("🔔", event.messageID, (err) => {}, true);
+    author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
+    role: 0,
+    category: "no prefix",
+    shortDescription: {
+      en: "🔔 𝙎𝙪𝙗𝙨𝙘𝙧𝙞𝙗𝙚 𝙨𝙖𝙢𝙥𝙖𝙧𝙠𝙚 𝙘𝙤𝙢𝙢𝙖𝙣𝙙"
+    },
+    longDescription: {
+      en: "𝘈𝘶𝘵𝘰-𝘳𝘦𝘴𝘱𝘰𝘯𝘥 𝘸𝘪𝘵𝘩 𝘴𝘶𝘣𝘴𝘤𝘳𝘪𝘱𝘵𝘪𝘰𝘯 𝘮𝘦𝘴𝘴𝘢𝘨𝘦 𝘸𝘩𝘦𝘯 𝘵𝘳𝘪𝘨𝘨𝘦𝘳 𝘸𝘰𝘳𝘥𝘴 𝘢𝘳𝘦 𝘥𝘦𝘵𝘦𝘤𝘵𝘦𝘥"
+    },
+    guide: {
+      en: ""
+    },
+    countDown: 5,
+    dependencies: {
+      "fs-extra": ""
     }
-}
+  },
 
-module.exports.onStart = function({ api, event, client, __GLOBAL }) {
-    // No additional functionality needed
-}
+  onChat: async function({ api, event, message }) {
+    try {
+      const { threadID, messageID, body } = event;
+
+      const triggerWords = [
+        "priyansh rajput",
+        "sub", 
+        "subscribe",
+        "priyansh"
+      ];
+
+      const lowerBody = body.toLowerCase();
+      
+      if (triggerWords.some(word => lowerBody.includes(word.toLowerCase()))) {
+        const msg = {
+          body: "👋 𝙆𝙤𝙣𝙤 𝙨𝙖𝙝𝙖𝙮𝙮𝙖 𝙡𝙖𝙜𝙡𝙚 @𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝙪𝒅 𝙠𝙚 𝙘𝙤𝙣𝙩𝙖𝙘𝙩 𝙠𝙤𝙧𝙪𝙣 😇",
+          attachment: fs.createReadStream(__dirname + `/noprefix/sub.mp3`)
+        };
+        
+        await message.reply(msg);
+        api.setMessageReaction("🔔", messageID, (err) => {}, true);
+      }
+    } catch (error) {
+      console.error("Sub command error:", error);
+    }
+  },
+
+  onStart: async function({ api, event, message }) {
+    // No additional functionality needed for start command
+    // This is primarily a no-prefix/onChat command
+  }
+};
