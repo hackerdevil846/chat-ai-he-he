6fada171520be9579751f99b2d502c3b1d2ae4e1
hackerdevil846 <hackerdevil846@gmail.com>
2025-09-19 16:17:06 +0600

    Update women.js

--- scripts/cmds/women.js
+++ scripts/cmds/women.js
@@ -1,18 +1,29 @@
-const fs = require("fs");
+const fs = require("fs-extra");
 
 module.exports = {
   config: {
     name: "women",
+    aliases: ["womentrigger", "femaletrigger"],
     version: "1.0.1",
-    Permssion: 0,
-    credits: "𝑨𝒔𝒊𝒇 𝑴𝒂𝒉𝒎𝒖𝒅",
-    description: "𝑴𝒐𝒉𝒊𝒍𝒂𝒅𝒆𝒓 𝒋𝒐𝒏𝒏𝒐 𝒆𝒌𝒕𝒂 𝒇𝒖𝒏𝒏𝒚 𝒄𝒐𝒎𝒎𝒂𝒏𝒅",
-    category: "no prefix",
-    usages: "𝑾𝒐𝒎𝒆𝒏",
-    cooldowns: 5
+    author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
+    role: 0,
+    category: "fun",
+    shortDescription: {
+      en: "☕ 𝑊𝑜𝑚𝑒𝑛 𝑡𝑟𝑖𝑔𝑔𝑒𝑟 𝑐𝑜𝑚𝑚𝑎𝑛𝑑"
+    },
+    longDescription: {
+      en: "𝐴𝑢𝑡𝑜-𝑟𝑒𝑠𝑝𝑜𝑛𝑑 𝑡𝑜 𝑤𝑜𝑚𝑒𝑛-𝑟𝑒𝑙𝑎𝑡𝑒𝑑 𝑘𝑒𝑦𝑤𝑜𝑟𝑑𝑠 𝑤𝑖𝑡ℎ 𝑎 𝑓𝑢𝑛𝑛𝑦 𝑣𝑖𝑑𝑒𝑜"
+    },
+    guide: {
+      en: "𝐽𝑢𝑠𝑡 𝑡𝑦𝑝𝑒 '𝑤𝑜𝑚𝑒𝑛' 𝑜𝑟 '☕' 𝑖𝑛 𝑡ℎ𝑒 𝑐ℎ𝑎𝑡"
+    },
+    countDown: 5,
+    dependencies: {
+      "fs-extra": ""
+    }
   },
 
-  handleEvent: function({ api, event }) {
+  onChat: async function({ event, message }) {
     const { threadID, messageID, body } = event;
     if (!body) return;
 
@@ -22,14 +33,16 @@ module.exports = {
       body.indexOf("WOMEN") === 0 ||
       body.indexOf("☕") === 0
     ) {
-      const msg = {
-        body: "Hahaha Mohila 🤣☕",
-        attachment: fs.createReadStream(__dirname + "/noprefix/wn.mp4")
-      };
-      api.sendMessage(msg, threadID, messageID);
-      api.setMessageReaction("☕", messageID, () => {}, true);
+      try {
+        const msg = {
+          body: "𝐻𝑎ℎ𝑎ℎ𝑎 𝑀𝑜ℎ𝑖𝑙𝑎 🤣☕",
+          attachment: fs.createReadStream(__dirname + "/noprefix/wn.mp4")
+        };
+        await message.reply(msg);
+        message.react("☕");
+      } catch (error) {
+        console.error("Women command error:", error);
+      }
     }
-  },
-
-  onStart: function () {}
+  }
 };
