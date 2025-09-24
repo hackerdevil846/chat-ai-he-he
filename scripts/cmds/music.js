0f1f5c7f013217fc70e5b672e319ed387a7da382
hackerdevil846 <hackerdevil846@gmail.com>
2025-09-25 03:33:28 +0600

    Update music.js

--- scripts/cmds/music.js
+++ scripts/cmds/music.js
@@ -7,7 +7,7 @@ const ytSearch = require("yt-search");
 module.exports = {
     config: {
         name: "music",
-        aliases: ["song", "ytmusic"],
+        aliases: ["ytdownload", "ytaudio"],
         version: "1.1.0",
         role: 0,
         author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
