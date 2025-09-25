cbee37d34662a57b618e1dd53b31fe5012ddf626
hackerdevil846 <hackerdevil846@gmail.com>
2025-09-25 18:14:36 +0600

    Update student.js

--- scripts/cmds/student.js
+++ scripts/cmds/student.js
@@ -5,7 +5,7 @@ const { loadImage, createCanvas } = require("canvas");
 module.exports = {
   config: {
     name: "student",
-    aliases: ["studentboard", "board"],
+    aliases: ["studentboard"],
     version: "3.1.1",
     author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
     role: 0,
