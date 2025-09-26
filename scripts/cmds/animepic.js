9199714db12b4178cf8a61c4138883a80b25bfd9
hackerdevil846 <hackerdevil846@gmail.com>
2025-09-26 09:53:53 +0600

    Update animepic.js

--- scripts/cmds/animepic.js
+++ scripts/cmds/animepic.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   config: {
     name: "animepic",
-    aliases: ["animeimage", "animeart"],
+    aliases: [],
     version: "1.0.0",
     author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
     role: 0,
