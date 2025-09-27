1922e8dbc94b043d056ecda10a5d95b010d43e67
hackerdevil846 <hackerdevil846@gmail.com>
2025-09-28 01:33:23 +0600

    Update fish.js

--- scripts/cmds/fish.js
+++ scripts/cmds/fish.js
@@ -1,63 +1,59 @@
-module.exports.config = {
-    name: "fish",
-    aliases: ["fishing", "machdhora"],
-    version: "1.0.0",
-    author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
-    countDown: 5,
-    role: 0,
-    category: "economy",
-    shortDescription: {
-        en: "🎣 𝐹𝑖𝑠ℎ 𝑎𝑛𝑑 𝑒𝑎𝑟𝑛 𝑚𝑜𝑛𝑒𝑦"
-    },
-    longDescription: {
-        en: "🎣 𝐶𝑎𝑡𝑐ℎ 𝑓𝑖𝑠ℎ 𝑎𝑛𝑑 𝑠𝑒𝑙𝑙 𝑡ℎ𝑒𝑚 𝑡𝑜 𝑒𝑎𝑟𝑛 𝑚𝑜𝑛𝑒𝑦"
-    },
-    guide: {
-        en: "{p}fish"
-    },
-    envConfig: {
-        cooldownTime: 1000000
-    }
-};
-
-module.exports.languages = {
-    "en": {
-        "cooldown": "⏰ | 𝑌𝑜𝑢 𝑎𝑙𝑟𝑒𝑎𝑑𝑦 𝑓𝑖𝑠ℎ𝑒𝑑 𝑡𝑜𝑑𝑎𝑦!\n🔁 | 𝑊𝑎𝑖𝑡 %1 𝑚𝑖𝑛𝑢𝑡𝑒𝑠 %2 𝑠𝑒𝑐𝑜𝑛𝑑𝑠 𝑡𝑜 𝑓𝑖𝑠ℎ 𝑎𝑔𝑎𝑖𝑛",
-        "rewarded": "🎣 | 𝑌𝑜𝑢 𝑐𝑎𝑢𝑔ℎ𝑡 𝑎 𝑟𝑎𝑟𝑒 %1!\n💰 | 𝑆𝑎𝑙𝑒 𝑝𝑟𝑖𝑐𝑒: %2$",
-        "fishing": "🐟 𝐹𝑖𝑠ℎ"
-    }
-};
-
-module.exports.onStart = async function({ event, api, Currencies, getText }) {
-    try {
-        const { threadID, messageID, senderID } = event;
-        const cooldown = global.configModule[this.config.name].cooldownTime;
-        let userData = await Currencies.getData(senderID);
-        
-        let data = userData.data || {};
-
-        if (typeof data !== "undefined" && data.fishTime && cooldown - (Date.now() - data.fishTime) > 0) {
-            const time = cooldown - (Date.now() - data.fishTime);
-            const minutes = Math.floor(time / 60000);
-            const seconds = Math.floor((time % 60000) / 1000);
-            
-            return api.sendMessage(getText("cooldown", minutes, seconds), threadID, messageID);
+module.exports = {
+    config: {
+        name: "fish",
+        aliases: ["fishing", "machdhora"],
+        version: "1.0.0",
+        author: "𝗔𝘀𝗶𝗳 𝗠𝗮𝗵𝗺𝘂𝗱",
+        countDown: 5,
+        role: 0,
+        category: "𝐄𝐂𝐎𝐍𝐎𝐌𝐘",
+        shortDescription: {
+            en: "🎣 𝐅𝐈𝐒𝐇 𝐀𝐍𝐃 𝐄𝐀𝐑𝐍 𝐌𝐎𝐍𝐄𝐘"
+        },
+        longDescription: {
+            en: "🎣 𝐂𝐀𝐓𝐂𝐇 𝐅𝐈𝐒𝐇 𝐀𝐍𝐃 𝐒𝐄𝐋𝐋 𝐓𝐇𝐄𝐌 𝐓𝐎 𝐄𝐀𝐑𝐍 𝐌𝐎𝐍𝐄𝐘"
+        },
+        guide: {
+            en: "{p}𝐟𝐢𝐬𝐡"
+        },
+        envConfig: {
+            cooldownTime: 1000000
         }
+    },
 
-        const amount = Math.floor(Math.random() * 1000000);
-        const rareFishes = ["🐋 𝑊ℎ𝑎𝑙𝑒", "🦈 𝑆ℎ𝑎𝑟𝑘", "🐠 𝐶𝑜𝑟𝑎𝑙 𝐹𝑖𝑠ℎ", "🦑 𝑂𝑐𝑡𝑜𝑝𝑢𝑠", "🐡 𝐵𝑙𝑜𝑤𝑓𝑖𝑠ℎ"];
-        const rareFish = rareFishes[Math.floor(Math.random() * rareFishes.length)];
+    onStart: async function({ event, message, usersData }) {
+        try {
+            const { senderID } = event;
+            const cooldown = this.config.envConfig.cooldownTime;
+            let userData = await usersData.get(senderID);
+            
+            let data = userData.data || {};
 
-        await Currencies.increaseMoney(senderID, amount);
-        
-        if (!userData.data) userData.data = {};
-        userData.data.fishTime = Date.now();
-        await Currencies.setData(senderID, userData);
+            if (typeof data !== "undefined" && data.fishTime && cooldown - (Date.now() - data.fishTime) > 0) {
+                const time = cooldown - (Date.now() - data.fishTime);
+                const minutes = Math.floor(time / 60000);
+                const seconds = Math.floor((time % 60000) / 1000);
+                
+                return message.reply(`⏰ | 𝐘𝐎𝐔 𝐀𝐋𝐑𝐄𝐀𝐃𝐘 𝐅𝐈𝐒𝐇𝐄𝐃 𝐓𝐎𝐃𝐀𝐘!\n🔁 | 𝐖𝐀𝐈𝐓 ${minutes} 𝐌𝐈𝐍𝐔𝐓𝐄𝐒 ${seconds} 𝐒𝐄𝐂𝐎𝐍𝐃𝐒 𝐓𝐎 𝐅𝐈𝐒𝐇 𝐀𝐆𝐀𝐈𝐍`);
+            }
 
-        return api.sendMessage(getText("rewarded", rareFish, amount), threadID, messageID);
-        
-    } catch (error) {
-        console.error("𝐹𝑖𝑠ℎ𝑖𝑛𝑔 𝐸𝑟𝑟𝑜𝑟:", error);
-        api.sendMessage("❌ 𝐴𝑛 𝑒𝑟𝑟𝑜𝑟 𝑜𝑐𝑐𝑢𝑟𝑟𝑒𝑑 𝑤ℎ𝑖𝑙𝑒 𝑓𝑖𝑠ℎ𝑖𝑛𝑔", event.threadID, event.messageID);
+            const amount = Math.floor(Math.random() * 1000000);
+            const rareFishes = ["🐋 𝐖𝐇𝐀𝐋𝐄", "🦈 𝐒𝐇𝐀𝐑𝐊", "🐠 𝐂𝐎𝐑𝐀𝐋 𝐅𝐈𝐒𝐇", "🦑 𝐎𝐂𝐓𝐎𝐏𝐔𝐒", "🐡 𝐁𝐋𝐎𝐖𝐅𝐈𝐒𝐇"];
+            const rareFish = rareFishes[Math.floor(Math.random() * rareFishes.length)];
+
+            await usersData.set(senderID, {
+                money: userData.money + amount,
+                data: {
+                    ...userData.data,
+                    fishTime: Date.now()
+                }
+            });
+
+            return message.reply(`🎣 | 𝐘𝐎𝐔 𝐂𝐀𝐔𝐆𝐇𝐓 𝐀 𝐑𝐀𝐑𝐄 ${rareFish}!\n💰 | 𝐒𝐀𝐋𝐄 𝐏𝐑𝐈𝐂𝐄: $${amount}`);
+            
+        } catch (error) {
+            console.error("𝐅𝐈𝐒𝐇𝐈𝐍𝐆 𝐄𝐑𝐑𝐎𝐑:", error);
+            message.reply("❌ 𝐀𝐍 𝐄𝐑𝐑𝐎𝐑 𝐎𝐂𝐂𝐔𝐑𝐑𝐄𝐃 𝐖𝐇𝐈𝐋𝐄 𝐅𝐈𝐒𝐇𝐈𝐍𝐆");
+        }
     }
 };
