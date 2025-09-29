7baef050dffbc1625de8d5511abec498050cf1df
hackerdevil846 <hackerdevil846@gmail.com>
2025-09-29 15:23:00 +0600

    Update add.js

--- scripts/cmds/add.js
+++ scripts/cmds/add.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
     config: {
         name: "add",
-        aliases: ["upload", "mediaadd"],
+        aliases: [],
         version: "1.0.0",
         author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
         countDown: 5,
