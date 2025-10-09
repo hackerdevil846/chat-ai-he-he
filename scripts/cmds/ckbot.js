2aa9596ebea21f1ae3cacd1cbe9083ea77738888
hackerdevil846 <hackerdevil846@gmail.com>
2025-10-09 18:45:01 +0600

    Update ckbot.js

--- scripts/cmds/ckbot.js
+++ scripts/cmds/ckbot.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 module.exports = {
     config: {
         name: "ckbot",
-        aliases: ["info", "botinfo"],
+        aliases: [],
         version: "1.0.0",
         author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
         countDown: 4,
