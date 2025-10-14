8a412d245dfa6da801554cf32be2bcef5a6fa918
hackerdevil846 <hackerdevil846@gmail.com>
2025-10-14 14:50:28 +0600

    Update and rename Dowa.js to dowa.js

--- scripts/cmds/dowa.js
+++ scripts/cmds/dowa.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 module.exports = {
     config: {
         name: "dowa",
-        aliases: ["দোয়া", "islamic"],
+        aliases: [],
         version: "1.0.0",
         author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
         countDown: 5,
