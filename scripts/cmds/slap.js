2488f19dc62e34b4199db89f5f964d7b8955e6d0
hackerdevil846 <hackerdevil846@gmail.com>
2025-10-20 00:09:04 +0600

    Update slap.js

--- scripts/cmds/slap.js
+++ scripts/cmds/slap.js
@@ -4,7 +4,7 @@ const fs = require('fs-extra');
 module.exports = {
   config: {
     name: "slap",
-    aliases: ["slapuser", "pita"],
+    aliases: [],
     version: "1.0.0",
     author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
     role: 0,
