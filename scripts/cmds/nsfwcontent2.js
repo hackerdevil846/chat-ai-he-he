48d3a5f3d6cb98334da575156ccac0df48fc9808
hackerdevil846 <hackerdevil846@gmail.com>
2025-10-22 21:53:19 +0600

    Update nsfwcontent2.js

--- scripts/cmds/nsfwcontent2.js
+++ scripts/cmds/nsfwcontent2.js
@@ -3,24 +3,21 @@ const axios = require("axios");
 module.exports = {
     config: {
         name: "nsfwcontent2",
-        aliases: ["nsfw2"],
+        aliases: [],
         version: "1.0.0",
         role: 0,
-        author: "𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑",
-        category: "𝑎𝑑𝑢𝑙𝑡",
+        author: "𝐀𝐬𝐢𝐟 𝐌𝐚𝐡𝐦𝐮𝐝",
+        category: "𝐚𝐝𝐮𝐥𝐭",
         shortDescription: {
-            en: "𝐺𝑒𝑡 𝑟𝑎𝑛𝑑𝑜𝑚 𝑁𝑆𝐹𝑊 𝑐𝑜𝑛𝑡𝑒𝑛𝑡"
+            en: "𝐆𝐞𝐭 𝐫𝐚𝐧𝐝𝐨𝐦 𝐍𝐒𝐅𝐖 𝐜𝐨𝐧𝐭𝐞𝐧𝐭"
         },
         longDescription: {
-            en: "𝐺𝑒𝑡 𝑟𝑎𝑛𝑑𝑜𝑚 𝑁𝑆𝐹𝑊 𝑐𝑜𝑛𝑡𝑒𝑛𝑡 𝑤𝑖𝑡ℎ 𝑚𝑢𝑙𝑡𝑖𝑝𝑙𝑒 𝑐𝑎𝑡𝑒𝑔𝑜𝑟𝑖𝑒𝑠"
+            en: "𝐆𝐞𝐭 𝐫𝐚𝐧𝐝𝐨𝐦 𝐍𝐒𝐅𝐖 𝐜𝐨𝐧𝐭𝐞𝐧𝐭 𝐰𝐢𝐭𝐡 𝐦𝐮𝐥𝐭𝐢𝐩𝐥𝐞 𝐜𝐚𝐭𝐞𝐠𝐨𝐫𝐢𝐞𝐬"
         },
         guide: {
-            en: "{p}nsfwcontent2 [𝑐𝑎𝑡𝑒𝑔𝑜𝑟𝑦]"
+            en: "{p}nsfwcontent2 [𝐜𝐚𝐭𝐞𝐠𝐨𝐫𝐲]"
         },
-        countDown: 5,
-        dependencies: {
-            "axios": ""
-        }
+        countDown: 5
     },
 
     onStart: async function({ message, event, args }) {
@@ -29,18 +26,18 @@ module.exports = {
             try {
                 require("axios");
             } catch (e) {
-                return message.reply("❌ 𝑀𝑖𝑠𝑠𝑖𝑛𝑔 𝑑𝑒𝑝𝑒𝑛𝑑𝑒𝑛𝑐𝑦: 𝑎𝑥𝑖𝑜𝑠");
+                return message.reply("❌ 𝐌𝐢𝐬𝐬𝐢𝐧𝐠 𝐝𝐞𝐩𝐞𝐧𝐝𝐞𝐧𝐜𝐲: 𝐚𝐱𝐢𝐨𝐬");
             }
 
             // Available categories
             const categories = {
-        'neko': 'https://api.waifu.pics/nsfw/neko',
-        'waifu': 'https://api.waifu.pics/nsfw/waifu',
-        'blowjob': 'https://api.waifu.pics/nsfw/blowjob',
-        'hentai': 'https://nekobot.xyz/api/image?type=hentai',
-        'anal': 'https://nekobot.xyz/api/image?type=anal',
-        'pgif': 'https://nekobot.xyz/api/image?type=pgif'
-      };
+                'neko': 'https://api.waifu.pics/nsfw/neko',
+                'waifu': 'https://api.waifu.pics/nsfw/waifu',
+                'blowjob': 'https://api.waifu.pics/nsfw/blowjob',
+                'hentai': 'https://nekobot.xyz/api/image?type=hentai',
+                'anal': 'https://nekobot.xyz/api/image?type=anal',
+                'pgif': 'https://nekobot.xyz/api/image?type=pgif'
+            };
 
             let category = args[0] || 'random';
             
@@ -51,24 +48,33 @@ module.exports = {
 
             if (!categories[category]) {
                 const availableCategories = Object.keys(categories).join(', ');
-                return message.reply(`❌ 𝐼𝑛𝑣𝑎𝑙𝑖𝑑 𝑐𝑎𝑡𝑒𝑔𝑜𝑟𝑦! 𝐴𝑣𝑎𝑖𝑙𝑎𝑏𝑙𝑒: ${availableCategories}`);
+                return message.reply(`❌ 𝐈𝐧𝐯𝐚𝐥𝐢𝐝 𝐜𝐚𝐭𝐞𝐠𝐨𝐫𝐲! 𝐀𝐯𝐚𝐢𝐥𝐚𝐛𝐥𝐞: ${availableCategories}`);
             }
 
-            await message.reply(`🔞 𝐿𝑜𝑎𝑑𝑖𝑛𝑔 ${category} 𝑐𝑜𝑛𝑡𝑒𝑛𝑡...`);
+            await message.reply(`🔞 𝐋𝐨𝐚𝐝𝐢𝐧𝐠 ${category} 𝐜𝐨𝐧𝐭𝐞𝐧𝐭...`);
 
             const response = await axios.get(categories[category]);
-            const imageUrl = response.data.url;
+            
+            // Handle different API response structures
+            let imageUrl;
+            if (response.data.url) {
+                imageUrl = response.data.url;
+            } else if (response.data.message) {
+                imageUrl = response.data.message;
+            } else {
+                throw new Error("❌ 𝐍𝐨 𝐢𝐦𝐚𝐠𝐞 𝐟𝐨𝐮𝐧𝐝");
+            }
 
-            if (!imageUrl) throw new Error("❌ 𝑁𝑜 𝑖𝑚𝑎𝑔𝑒 𝑓𝑜𝑢𝑛𝑑");
+            if (!imageUrl) throw new Error("❌ 𝐍𝐨 𝐢𝐦𝐚𝐠𝐞 𝐟𝐨𝐮𝐧𝐝");
 
             await message.reply({
-                body: `🥵 ${category.toUpperCase()} 𝑁𝑆𝐹𝑊 𝐶𝑜𝑛𝑡𝑒𝑛𝑡\n━━━━━━━━━━━━━━\n✨ 𝐶𝑟𝑒𝑑𝑖𝑡: 𝐴𝑠𝑖𝑓 𝑀𝑎ℎ𝑚𝑢𝑑`,
+                body: `🥵 ${category.toUpperCase()} 𝐍𝐒𝐅𝐖 𝐂𝐨𝐧𝐭𝐞𝐧𝐭\n━━━━━━━━━━━━━━\n✨ 𝐂𝐫𝐞𝐝𝐢𝐭: 𝐀𝐬𝐢𝐟 𝐌𝐚𝐡𝐦𝐮𝐝`,
                 attachment: await global.utils.getStreamFromURL(imageUrl)
             });
 
         } catch (error) {
-            console.error("𝑁𝑆𝐹𝑊 𝐸𝑟𝑟𝑜𝑟:", error);
-            await message.reply("❌ 𝐸𝑟𝑟𝑜𝑟 𝑓𝑒𝑡𝑐ℎ𝑖𝑛𝑔 𝑐𝑜𝑛𝑡𝑒𝑛𝑡: " + error.message);
+            console.error("𝐍𝐒𝐅𝐖 𝐄𝐫𝐫𝐨𝐫:", error);
+            await message.reply("❌ 𝐄𝐫𝐫𝐨𝐫 𝐟𝐞𝐭𝐜𝐡𝐢𝐧𝐠 𝐜𝐨𝐧𝐭𝐞𝐧𝐭: " + error.message);
         }
     }
 };
